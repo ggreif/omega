5b3ef0754a6226a39ddb682c4d30185377d6c14b
Tim Sheard <sheard@cs.pdx.edu>
2007-04-16 17:51:00 +0000

    import Omega v1.4.1+ snapshot 2007-04-16

--- Version.hs
+++ Version.hs
@@ -2,9 +2,9 @@
 -- OGI School of Science & Engineering, Oregon Health & Science University
 -- Maseeh College of Engineering, Portland State University
 -- Subject to conditions of distribution and use; see LICENSE.txt for details.
--- Thu Apr 12 15:30:57 Pacific Daylight Time 2007
+-- Mon Apr 16 10:51:51 Pacific Daylight Time 2007
 -- Omega Interpreter: version 1.4.1
 
 module Version where
 version = "Omega Interpreter: version 1.4.1"
-buildtime = "Thu Apr 12 15:30:57 Pacific Daylight Time 2007"
+buildtime = "Mon Apr 16 10:51:51 Pacific Daylight Time 2007"
