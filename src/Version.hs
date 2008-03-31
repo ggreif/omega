487184ac0f1c63bcdc7316c9336d22f593738d7c
Tim Sheard <sheard@cs.pdx.edu>
2008-03-31 15:10:35 +0000

    fixing bugs for making the manual in the Makefile

--- src/Version.hs
+++ src/Version.hs
@@ -1,3 +1,3 @@
 module Version where
 version = "Omega Interpreter: version 1.4.3"
-buildtime = "Mon Mar 31 07:50:54 Pacific Daylight Time 2008"
+buildtime = "Mon Mar 31 07:55:46 Pacific Daylight Time 2008"
