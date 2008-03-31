666e8cc4480f29d370abde830efd5b983c0bf37e
Gabor Greif <ggreif@users.noreply.github.com>
2008-03-31 14:07:44 +0000

    remove headers with copyright notice. they will be resupplied when building tarball

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,3 @@
--- Copyright (c) Tim Sheard
--- OGI School of Science & Engineering, Oregon Health & Science University
--- Maseeh College of Engineering, Portland State University
--- Subject to conditions of distribution and use; see LICENSE.txt for details.
--- Mon Mar 31 02:56:16 Pacific Daylight Time 2008
--- Omega Interpreter: version 1.4.2
 
 module Main where
 
