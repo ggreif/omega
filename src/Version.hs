cfb81fe5803f0625c4958934fb4c394ca9d43479
Tim Sheard <sheard@cs.pdx.edu>
2008-06-27 19:57:19 +0000

    Fixed a small bug when you typed
    :t {plus Z Z}
    This caused a pattern match failure in the source code,
    but it should have caught a parsing error gracefully.

--- src/Version.hs
+++ src/Version.hs
@@ -1,3 +1,3 @@
 module Version where
 version = "Omega Interpreter: version 1.4.3"
-buildtime = "Mon Mar 31 08:13:29 Pacific Daylight Time 2008"
+buildtime = "Thu May  8 10:41:47 Pacific Daylight Time 2008"
