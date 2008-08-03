496f088b76302f676f15a035d0008818c58f80c7
sheardOmega <sheardOmega@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2008-08-03 04:19:34 +0000

    Don't know what changed, probably a Bugs

--- trunk/src/Version.hs
+++ trunk/src/Version.hs
@@ -1,3 +1,3 @@
 module Version where
 version = "Omega Interpreter: version 1.4.3"
-buildtime = "Sat Jul 26 08:58:26 Pacific Daylight Time 2008"
+buildtime = "Thu Jul 31 23:22:09 Pacific Daylight Time 2008"
