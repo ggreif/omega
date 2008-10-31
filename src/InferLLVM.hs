dc08264fbf9290d11e08bb0cabd42a7d638354d4
Gabor Greif <ggreif@users.noreply.github.com>
2008-10-31 23:18:19 +0000

    some minute progress with declaring Exp to be typable by LLType

--- src/InferLLVM.hs
+++ src/InferLLVM.hs
@@ -14,21 +14,35 @@ data LLType where
   LLInt :: LLType
   LLPtr :: LLType
   LLPair :: LLType -> LLType -> LLType
+  LLTv :: TcTv -> LLType
 
 
 instance (HasIORef m,Fresh m) => Zonk m LLType where
  zonkG = zonkLL
- tvs = undefined
+ tvs = tvs_LL
 
 
-zonkLL:: (HasIORef m,Fresh m) => LLType -> m LLType
---zonkLL (Rarrow x y) = binaryLift arrow (zonkSigma x) (zonkRho y)
+zonkLL :: (HasIORef m,Fresh m) => LLType -> m LLType
+zonkLL a@LLInt = return a
+zonkLL a@LLPtr = return a
 zonkLL (LLPair x y) = binaryLift pair (zonkLL x) (zonkLL y)
---zonkLL (Rsum x y) = binaryLift rsum (zonkSigma x) (zonkSigma y)
---zonkLL (Rtau x) = do { w <- zonkTau x; return(Rtau w)}
+zonkLL (LLTv (Tv un flav k)) = do { k2 <- zonkKind k; return (LLTv (Tv un flav k2)) }
+
 pair :: LLType -> LLType -> LLType
 pair x y = LLPair x y
 
+tvs_LL :: (HasIORef m,Fresh m) => LLType -> m([TcTv],[TcLv])
+tvs_LL LLInt = return ([], [])
+tvs_LL LLPtr = return ([], [])
+tvs_LL (LLPair x y) = binaryLift unionP (tvs_LL x) (tvs_LL y)
+--tvs_LL (LLTv v) = tvs v
 
 instance TyCh m => TypeLike m LLType where
   sub = undefined
+
+
+instance (TypeLike m LLType,TyCh m) => Typable m Exp LLType where
+  tc = undefined -- :: term -> Expected ty -> m term
+  check = undefined -- :: term -> ty -> m term
+  infer = undefined -- :: term -> m (ty,term)
+
