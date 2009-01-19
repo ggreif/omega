c4a042cd8acc6d3ab0749be1d66d97236ee96300
Gabor Greif <ggreif@users.noreply.github.com>
2009-01-19 17:18:26 +0000

    Re-enable readline functionality by using the System.Console.Editline.Readline compatibility module. This is a GHC 6.10.1-related fix. Fixes issue 61.

--- src/Toplevel.hs
+++ src/Toplevel.hs
@@ -31,9 +31,6 @@ import Manual(makeManual)
 import Commands
 import SyntaxExt(synName,synKey)
 
--- import System.Console.Readline(setCompletionEntryFunction)
--- setCompletionEntryFunction :: Maybe (String -> IO [String]) -> IO ()
-
 -------------------------------------------------------------
 -- The programmer interface: the top level loop.
 -- it performs the read-eval-typecheck-print loop.
