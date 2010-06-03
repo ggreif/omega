78228f0088005f4ae6c1e51a77edfaa81cbf6955
sheardOmega <sheardOmega@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2010-06-03 22:15:31 +0000

    Fixed a bug in printing types that used the list syntax extension.

--- trunk/src/Version.hs
+++ trunk/src/Version.hs
@@ -1,3 +1,3 @@
 module Version where
-version = "Omega Interpreter: version 1.4.5"
-buildtime = "Wed Apr 14 13:36:19 Pacific Daylight Time 2010"
+version = "Omega Interpreter: version 1.4.6"
+buildtime = "Fri May  7 10:18:58 Pacific Daylight Time 2010"
