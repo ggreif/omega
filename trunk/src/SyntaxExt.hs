28930b1039e0848a222f969174f6779466737a3d
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2010-12-06 10:16:05 +0000

    patch up (Listx (Right []) Nothing s) to match (Ix(t,Just(Left _),_,_,_,_))

--- trunk/src/SyntaxExt.hs
+++ trunk/src/SyntaxExt.hs
@@ -206,6 +206,7 @@ matchExt loc (Numx _ _ s)    (Ix(t,_,Just _,_,_,_)) | s==t = return True
 matchExt loc (Pairx _ s)     (Ix(t,_,_,Just _,_,_)) | s==t = return True
 matchExt loc (Recordx _ _ s) (Ix(t,_,_,_,Just _,_)) | s==t = return True
 matchExt loc (Tickx _ _ s)   (Ix(t,_,_,_,_,Just _)) | s==t = return True
+matchExt loc (Listx (Right []) Nothing s)   (Ix(t,Just(Left _),_,_,_,_)) | s==t = return True
 matchExt loc _ _                = return False
 
 ----------------------------------------------------------
@@ -241,6 +242,7 @@ buildExt loc (lift0,lift1,lift2,lift3) x ys =
         (Listx (Left xs) (Just x) _,Ix(tag,Just(Left(nil,cons)),_,_,_,_)) -> error "buildExt SNOC just" --return(x: foldr (lift2 cons) [] xs)
 	(Listx (Right xs) Nothing  _,Ix(tag,Just(Right(nil,cons)),_,_,_,_)) -> return(foldr (lift2 cons) (lift0 nil) xs)
         (Listx (Left xs) Nothing  _,Ix(tag,Just(Left(nil,cons)),_,_,_,_)) -> error "buildExt SNOC nothing" --return(lift0 nil: foldr (lift2 cons) [] xs)
+        (Listx (Right []) Nothing  _,Ix(tag,Just(Left(nil,cons)),_,_,_,_)) -> return(lift0 nil)
         (Recordx xs (Just x) _,Ix(tag,_,_,_,Just(nil,cons),_)) -> return(foldr (uncurry(lift3 cons)) x xs)
         (Recordx xs Nothing  _,Ix(tag,_,_,_,Just(nil,cons),_)) -> return(foldr (uncurry(lift3 cons)) (lift0 nil) xs)
         (Tickx n x _,Ix(tag,_,_,_,_,Just tick)) -> return(buildNat x (lift1 tick) n)
