9b369798a43aaed7597f5c367f07a37e895908b1
Gabor Greif <ggreif@users.noreply.github.com>
2010-12-06 22:52:16 +0000

    preparing for new release

--- src/Version.hs
+++ src/Version.hs
@@ -1,3 +1,3 @@
 module Version where
-version = "Omega Interpreter: version 1.4.6"
-buildtime = "Fri May  7 10:18:58 Pacific Daylight Time 2010"
+version = "Omega Interpreter: version 1.4.7pre"
+buildtime = "unspecified"
