82789d264880941ec7b47dbcfca177727dcc2aed
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2011-04-29 15:16:24 +0000

    birthday present: v1.5

--- trunk/src/Version.hs
+++ trunk/src/Version.hs
@@ -1,3 +1,3 @@
 module Version where
-version = "Omega Interpreter: version 1.4.7pre"
-buildtime = "unspecified"
+version = "Omega Interpreter: version 1.5"
+buildtime = "Fri Apr 29 06:04:04 PDT 2011"
