9146e6e7b3e4d1ca9743deafdaed38e332f48d23
Gabor Greif <ggreif@users.noreply.github.com>
2011-04-29 13:49:44 +0000

    merge branches/release-improvements to trunk

--- util/BuildSpecific.hs
+++ util/BuildSpecific.hs
@@ -3,12 +3,15 @@ module BuildSpecific where
 -- ====================================================================
 -- Everytime you make a distribution you need to edit the following
 
-version = "Omega Interpreter: version 1.4.6"
+version = "Omega Interpreter: version 1.5"
 
-distrDir = "D:/Omega/distr/"
-srcDir = "D:/Omega/src/"
-parseDir = "D:/Omega/src/"
-libDir = "D:/Omega/src/"
-manualDir = "D:/Omega/doc/"
-testsDir = "D:/Omega/tests/"
-rootDir = "D:/Omega/"
+distrDir = "/home/gabor/Omega/distr/"
+
+rootDir = "/home/gabor/omega/"
+srcDir = rootDir++"src/"
+parseDir = rootDir++"src/"
+libDir = rootDir++"src/"
+manualDir = rootDir++"doc/"
+testsDir = rootDir++"tests/"
+
+extension = ""
