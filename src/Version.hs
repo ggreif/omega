1736596bd88a51c82e52fed8c8b8d62837806fe1
Gabor Greif <ggreif@users.noreply.github.com>
2011-04-29 15:27:10 +0000

    this is a development version

--- src/Version.hs
+++ src/Version.hs
@@ -1,3 +1,3 @@
 module Version where
-version = "Omega Interpreter: version 1.5"
-buildtime = "Fri Apr 29 06:04:04 PDT 2011"
+version = "Omega Interpreter: version 1.5.1pre"
+buildtime = "unspecified"
