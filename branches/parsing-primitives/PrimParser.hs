b9e6d348e595dfe13fb76a6a97f68261c609b589
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2011-08-19 20:32:56 +0000

    refactor

--- branches/parsing-primitives/PrimParser.hs
+++ branches/parsing-primitives/PrimParser.hs
@@ -24,19 +24,15 @@ instance Encoding a => Encoding (Parser a) where
 
 instance (Encoding a,Encoding b) => Encoding (a -> Parser b) where
     to f = Vprimfun "->" (return . to . f . from)
-    from (Vprimfun s f) = from . help . f . to
+    from f = from . help . (getf f) . to
       where help :: FIO V -> V
             help (FIO w) = case unsafePerformIO w of
                            Ok (v@(Vlazy _ _)) -> help(analyzeWith return v)
                            Ok v -> v
                            Fail loc _ _ mess -> error("Near "++show loc++"\n"++mess)
-    from (Vf f _ _) = from . help . f . to
-      where help :: FIO V -> V
-            help (FIO w) = case unsafePerformIO w of
-                           Ok (v@(Vlazy _ _)) -> help(analyzeWith return v)
-                           Ok v -> v
-                           Fail loc _ _ mess -> error("Near "++show loc++"\n"++mess)
-    from v = error ("Value not a function: "++(show v))
+            getf (Vprimfun _ f) = f
+            getf (Vf f _ _) = f
+            getf v = error ("Value not a function: "++(show v))
 
 {-
 instance Encoding (Parser V) where
