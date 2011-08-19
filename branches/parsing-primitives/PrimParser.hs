af7e66d8f86d05a8c571616475c363908cfd1d70
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2011-08-19 20:16:42 +0000

    handle normal functions too, this works now: prompt> runParser (bindParser parseInt (\i->returnParser (lazy (i+1)))) 41
    (Just 42) : Maybe Int
    
    REFACTORING DUE :-)

--- branches/parsing-primitives/PrimParser.hs
+++ branches/parsing-primitives/PrimParser.hs
@@ -30,6 +30,12 @@ instance (Encoding a,Encoding b) => Encoding (a -> Parser b) where
                            Ok (v@(Vlazy _ _)) -> help(analyzeWith return v)
                            Ok v -> v
                            Fail loc _ _ mess -> error("Near "++show loc++"\n"++mess)
+    from (Vf f _ _) = from . help . f . to
+      where help :: FIO V -> V
+            help (FIO w) = case unsafePerformIO w of
+                           Ok (v@(Vlazy _ _)) -> help(analyzeWith return v)
+                           Ok v -> v
+                           Fail loc _ _ mess -> error("Near "++show loc++"\n"++mess)
     from v = error ("Value not a function: "++(show v))
 
 {-
