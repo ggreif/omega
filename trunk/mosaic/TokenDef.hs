6f56a9bbadc2cde9c128e8ac15443b05bf0817fa
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2011-12-21 19:40:28 +0000

    export even more useful definitions

--- trunk/mosaic/TokenDef.hs
+++ trunk/mosaic/TokenDef.hs
@@ -1,8 +1,14 @@
 module TokenDef ( tokenDef, omegaTokenParser
                 , TokenDef.identifier
+                , TokenDef.reserved
+                , TokenDef.operator
+                , TokenDef.reservedOp
+                , TokenDef.charLiteral
+                , TokenDef.stringLiteral
+                , TokenDef.natural
+                , TokenDef.integer
                 , TokenDef.braces
-                , TokenDef.parens
-                , TokenDef.reserved ) where
+                , TokenDef.parens ) where
 
 import Text.Parsec.Token
 import Text.Parsec.Language
@@ -33,3 +39,9 @@ parens = Text.Parsec.Token.parens omegaTokenParser
 braces = Text.Parsec.Token.braces omegaTokenParser
 identifier = Text.Parsec.Token.identifier omegaTokenParser
 reserved = Text.Parsec.Token.reserved omegaTokenParser
+operator = Text.Parsec.Token.operator omegaTokenParser
+reservedOp = Text.Parsec.Token.reservedOp omegaTokenParser
+charLiteral = Text.Parsec.Token.charLiteral omegaTokenParser
+stringLiteral = Text.Parsec.Token.stringLiteral omegaTokenParser
+natural = Text.Parsec.Token.natural omegaTokenParser
+integer = Text.Parsec.Token.integer omegaTokenParser
