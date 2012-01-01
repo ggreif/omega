3031c712008b90b79547d39176e159e87b5c0cc5
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2012-01-01 01:22:45 +0000

    gauge_member_fun is it

--- trunk/mosaic/gauge.cpp
+++ trunk/mosaic/gauge.cpp
@@ -8,14 +8,25 @@ template <typename RETURN, class RECEIVER, typename ARG1, typename ARG2>
 void gauge(RETURN (RECEIVER::*)(ARG1, ARG2));
 
 template <typename RETURN, class RECEIVER, typename ARG1, typename ARG2, typename ARG3>
-struct gauge3
+struct gauge_member_fun
 {
-  template <RETURN (RECEIVER::*)(ARG1, ARG2, ARG3)>
-  void regauge(void) const;
+  typedef const RETURN (*ref)(RECEIVER&, ARG1, ARG2, ARG3);
+
+  template <RETURN (RECEIVER::*MEMBER_FUN)(ARG1, ARG2, ARG3)>
+  struct regauge2
+  {
+    static const RETURN ref(RECEIVER& r, ARG1 a1, ARG2 a2, ARG3 a3) { return (r.*MEMBER_FUN)(a1, a2, a3); }
+  };
+
+  template <RETURN (RECEIVER::*MEMBER_FUN)(ARG1, ARG2, ARG3)>
+  ref regauge(void) const
+  {
+    return regauge2<MEMBER_FUN>::ref;
+  }
 };
 
 template <typename RETURN, class RECEIVER, typename ARG1, typename ARG2, typename ARG3>
-gauge3<RETURN, RECEIVER, ARG1, ARG2, ARG3> gauge(RETURN (RECEIVER::*)(ARG1, ARG2, ARG3));
+gauge_member_fun<RETURN, RECEIVER, ARG1, ARG2, ARG3> gauge(RETURN (RECEIVER::*)(ARG1, ARG2, ARG3));
 
 
 template <typename RETURN, typename ARG1, typename ARG2, typename ARG3>
@@ -55,16 +66,14 @@ struct Foo
   static char* bar3(int, Foo&, const char*);
 };
 
-// template void gauge2<char*, Foo, int, Foo&, const Foo*, &Foo::foo3>(__typeof__(&Foo::foo3));
-
-void gaugers(void)
-{
-  gauge(&Foo::foo0);
-  gauge(&Foo::foo1);
-  gauge(&Foo::foo2);
-  gauge(&Foo::foo3);
-  //  gauge2<char*, Foo, int, Foo&, &Foo::foo3>(&Foo::foo3);
-  gauge(&Foo::foo3).regauge<&Foo::foo3>();
-  gauge(&Foo::bar3);
-  gauge(&Foo::bla).regauge<&Foo::bla>();
+namespace {
+  void gaugers(void)
+  {
+    gauge(&Foo::foo0);
+    gauge(&Foo::foo1);
+    gauge(&Foo::foo2);
+    gauge(&Foo::foo3).regauge<&Foo::foo3>();
+    gauge(&Foo::bar3);
+    gauge(&Foo::bla).regauge<&Foo::bla>();
+  }
 }
