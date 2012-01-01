94d12500c70c3527dab1e2ffac52908e9b6945f2
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2012-01-01 14:11:55 +0000

    have constant member functions too

--- trunk/mosaic/gauge.cpp
+++ trunk/mosaic/gauge.cpp
@@ -10,12 +10,12 @@ void gauge(RETURN (RECEIVER::*)(ARG1, ARG2));
 template <typename RETURN, class RECEIVER, typename ARG1, typename ARG2, typename ARG3>
 struct gauge_member_fun
 {
-  typedef const RETURN (*ref)(RECEIVER&, ARG1, ARG2, ARG3);
+  typedef RETURN (*ref)(RECEIVER&, ARG1, ARG2, ARG3);
 
   template <RETURN (RECEIVER::*MEMBER_FUN)(ARG1, ARG2, ARG3)>
   struct regauge2
   {
-    static const RETURN ref(RECEIVER& r, ARG1 a1, ARG2 a2, ARG3 a3) { return (r.*MEMBER_FUN)(a1, a2, a3); }
+    static RETURN ref(RECEIVER& r, ARG1 a1, ARG2 a2, ARG3 a3) { return (r.*MEMBER_FUN)(a1, a2, a3); }
   };
 
   template <RETURN (RECEIVER::*MEMBER_FUN)(ARG1, ARG2, ARG3)>
@@ -26,7 +26,27 @@ struct gauge_member_fun
 };
 
 template <typename RETURN, class RECEIVER, typename ARG1, typename ARG2, typename ARG3>
+struct gauge_member_cfun
+{
+  typedef RETURN (*ref)(const RECEIVER&, ARG1, ARG2, ARG3);
+
+  template <RETURN (RECEIVER::*MEMBER_FUN)(ARG1, ARG2, ARG3) const>
+  struct regauge2
+  {
+    static RETURN ref(const RECEIVER& r, ARG1 a1, ARG2 a2, ARG3 a3) { return (r.*MEMBER_FUN)(a1, a2, a3); }
+  };
+
+  template <RETURN (RECEIVER::*MEMBER_FUN)(ARG1, ARG2, ARG3) const>
+  ref regauge(void) const
+  {
+    return regauge2<MEMBER_FUN>::ref;
+  }
+};
+
+template <typename RETURN, class RECEIVER, typename ARG1, typename ARG2, typename ARG3>
 gauge_member_fun<RETURN, RECEIVER, ARG1, ARG2, ARG3> gauge(RETURN (RECEIVER::*)(ARG1, ARG2, ARG3));
+template <typename RETURN, class RECEIVER, typename ARG1, typename ARG2, typename ARG3>
+gauge_member_cfun<RETURN, RECEIVER, ARG1, ARG2, ARG3> gauge(RETURN (RECEIVER::*)(ARG1, ARG2, ARG3) const);
 
 
 template <typename RETURN, typename ARG1, typename ARG2, typename ARG3>
@@ -63,17 +83,19 @@ struct Foo
   Foo& foo1(int*);
   void foo2(int*, Foo*);
   char* foo3(int, Foo&, const Foo*);
+  char* foo3c(int, Foo&, const Foo*) const;
   static char* bar3(int, Foo&, const char*);
 };
 
-namespace {
+//namespace {
   void gaugers(void)
   {
     gauge(&Foo::foo0);
     gauge(&Foo::foo1);
     gauge(&Foo::foo2);
     gauge(&Foo::foo3).regauge<&Foo::foo3>();
+    gauge(&Foo::foo3c).regauge<&Foo::foo3c>();
     gauge(&Foo::bar3);
     gauge(&Foo::bla).regauge<&Foo::bla>();
   }
-}
+//}
