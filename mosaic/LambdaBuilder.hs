42518fcae358dcdc4be8e289e1905b90075043f8
Gabor Greif <ggreif@users.noreply.github.com>
2013-07-11 21:03:25 +0000

    add the classical lambda calculus

--- mosaic/LambdaBuilder.hs
+++ mosaic/LambdaBuilder.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE KindSignatures, DataKinds, TypeOperators #-}
+{-# LANGUAGE KindSignatures, DataKinds, TypeOperators, StandaloneDeriving, GADTs #-}
 
 -- See: https://code.google.com/p/omega/wiki/LambdaGraph
 
@@ -11,3 +11,17 @@ class Builder (shape :: Lam -> *) where
   app :: shape left -> shape right -> shape (App left right)
   here :: shape (Ref '[Up])
   up :: shape (Ref p) -> shape (Ref (Up ': p))
+
+data Classical :: Lam -> * where
+  LAM :: Classical sh -> Classical (Abs sh)
+  APP :: Classical left -> Classical right -> Classical (App left right)
+  VAR :: Classical sh -> Classical (Var sh)
+  HERE :: Classical (Ref '[Up])
+
+deriving instance Show (Classical sh)
+
+instance Builder Classical where
+  lam = LAM
+  app = APP
+  v = VAR
+  here = HERE
