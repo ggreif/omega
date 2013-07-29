56378746e1df6ce2f58f415a3a95a891527fff60
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2013-07-29 08:26:11 +0000

    suggest something better even

--- trunk/mosaic/LambdaBuilder.hs
+++ trunk/mosaic/LambdaBuilder.hs
@@ -5,7 +5,7 @@
 -- See: https://code.google.com/p/omega/wiki/LambdaGraph
 -- TODO: model "let(rec) a = sub in expr" with KILL1 @ sub (expr ... UP LEFT)
 -- TODO: LEFT of LAMBDA
--- TODO: HERE = UP STOP
+-- TODO: HERE = UP STOP, even better: binder #n
 -- TODO: class Typed where itsType :: l sh -> levup l sh
 -- TODO: a simple minded interpreter (abstracted monadic?), compiler?
 
