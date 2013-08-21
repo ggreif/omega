b47867a680e3dc3e09b6ec7a999ecf85380f9e47
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2013-08-21 10:22:24 +0000

    update version to match distribution

--- tags/omega-1.5.2/src/Version.hs
+++ tags/omega-1.5.2/src/Version.hs
@@ -1,3 +1,3 @@
 module Version where
-version = "Omega Interpreter: version 1.5.2pre"
-buildtime = "unspecified"
+version = "Omega Interpreter: version 1.5.2"
+buildtime = "Wed Aug 21 12:06:10 CEST 2013"
