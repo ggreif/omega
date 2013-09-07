7dc1c683485c93593aa241d379849a09dba78842
Gabor Greif <ggreif@users.noreply.github.com>
2013-09-07 20:30:44 +0000

    checkpoint: fixed fallout of unsafeCast (mostly), seems to work better

--- src/ParserAll.hs
+++ src/ParserAll.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ViewPatterns, FlexibleContexts, FlexibleInstances
-           , GADTs, MultiParamTypeClasses #-}
+           , GADTs, MultiParamTypeClasses, StandaloneDeriving #-}
 
 module ParserAll
   ( module Text.Parsec.Combinator
@@ -13,9 +13,9 @@ module ParserAll
   , reservedOp', possible, parse2, integer, charLiteral, stringLiteral
   , layout, sourceName, sourceLine, sourceColumn, float, naturalOrFloat
   , isReservedName, prefixIdentifier, parseFromFile, opLetter, operator
-  , oper, parseFromHandle, getPosition, setPosition, Parser --, runParser
+  , oper, parseFromHandle, getPosition, {-setPosition,-} Parser --, runParser
 
-, res
+, res, res'
 
  ) where
 
@@ -30,28 +30,31 @@ import qualified Text.Parsec.Prim as Prim
 import Text.Parsec.Char
 import qualified Text.Parsec.Token as P
 import Text.Parsec.Expr
---import qualified Text.Parsec.Pos (SourcePos(..), SourceName, Line, Column) as Pos
 import qualified Text.Parsec.Pos as Pos
 import TokenDef
 import Data.Functor.Identity
 import System.IO (hGetContents, Handle)
 import Unsafe.Coerce (unsafeCoerce)
 import Control.Monad (guard)
+import Debug.Trace
 
 natNoSpace :: Parsec (Layout String Identity) u Int
 natNoSpace = fmap fromInteger natural -- TODO: fmap fromInteger nat -- do not eat space!!!!
 
---relax :: Stream s Identity Char => (P.LanguageDef u -> P.GenTokenParser String u Identity) -> (P.GenLanguageDef s u Identity -> P.GenTokenParser s u Identity)
 relax :: Stream s Identity Char => (P.LanguageDef u, P.GenLanguageDef s u Identity) -> P.GenLanguageDef s u Identity
 relax = unsafeCoerce fst
 
-
-
-----omegaTokens :: Stream (Layout String Identity) Identity Char => P.GenTokenParser s u Identity
 omegaTokens :: P.GenTokenParser (Layout String Identity) u Identity
-omegaTokens = P.makeTokenParser tokenDef' -- ---$ relax (tokenDef, undefined)
+omegaTokens = P.makeTokenParser tokenDef'
 
-tokenDef' = relax (tokenDef, undefined)
+tokenDef' = (relax (tokenDef, undefined))
+               { P.identStart = letter -- these do not cast benignly :-(
+               , P.identLetter = alphaNum <|> oneOf "_'"
+               , P.opStart = opLetters
+               --, P.opLetter = opLetters
+               }
+   where opLetters :: Parsec (Layout String Identity) u Char
+         opLetters = oneOf ":!#$%&*+./<=>?@\\^|-~"
 
 lexeme = P.lexeme omegaTokens
 comma = P.comma omegaTokens
@@ -82,35 +85,21 @@ ident = do { c <- P.identStart tokenDef'
 construct = do { c <- upper
                ; cs <- many (P.identLetter tokenDef')
                ; return (c:cs)
-               } <?> "Constructor name"
+               } <?> "constructor name"
 
 possible p = fmap Just (try p) <|> return Nothing
 
 runParser :: Parser a -> String -> Maybe a
 runParser = undefined4
 
-unitState :: Parsec (ParserAll.Layout String Identity) u a -> Parsec (ParserAll.Layout String Identity) () a
+unitState :: Parsec s u a -> Parsec s () a
 unitState = unsafeCoerce
 
-unitState' :: Parsec s u a -> Parsec s () a
-unitState' = unsafeCoerce
-
------parse2 :: Stream s Identity Char => Parsec s u p -> u -> Either String (p, String)
 parse2 :: Parsec (Layout String Identity) u p -> String -> Either String (p, String)
 parse2 p input
-    = case runP (unitState' (whiteSpace >> p)) () "keyboard input" (intoLayout input) of
+    = case runP (unitState (whiteSpace >> p)) () "keyboard input" (intoLayout input) of
         Left err -> Left (show err)
         Right p -> Right (p, "")
-
---        Ok x (State cs _ _) _ -> Right (x, cs)
---        Error err -> Left (show err)
-
-{-
-parserReply result
-    = case result of
-        Consumed reply -> reply
-        Empty reply -> reply
--}
 
 --------------------------------------------
 -- Haskell style combinators
@@ -126,27 +115,30 @@ explicitBrace = symbol "}" <?> "explicit layout closing brace"
 data SourcePos  = SourcePos { sourceName :: Pos.SourceName, sourceLine :: Pos.Line, sourceColumn :: Pos.Column, layoutTabs :: [Pos.Column] }
     deriving (Eq, Ord)
 
+getPosition :: Parsec (Layout String Identity) u SourcePos
 getPosition = do (sourceParts -> (cs, line, col)) <- Prim.getPosition
-                 return $ SourcePos cs line col []
+                 (Indent tabs col _ _) <- getInput
+                 return $ SourcePos cs line col tabs
     where sourceParts s = (Pos.sourceName s, Pos.sourceLine s, Pos.sourceColumn s)
 
-setPosition (SourcePos cs line col tabs) = Prim.setPosition $ Pos.newPos cs line col
+--setPosition (SourcePos cs line col tabs) = Prim.setPosition $ Pos.newPos cs line col
 
-indent :: Parsec s u ()
-indent =
-  do { SourcePos cs line col tabs <- getPosition
-     ; setPosition (SourcePos cs line col (col : tabs))
-     }
+indent :: Parsec (Layout String Identity) u ()
+indent = indent'
+--  do { SourcePos cs line col tabs <- getPosition
+--     ; setPosition (SourcePos cs line col (col : tabs))
+--     }
 
-undent :: Parsec s u ()
-undent =
-  do { SourcePos cs line col (p:ps) <- getPosition
-     ; setPosition (SourcePos cs line col ps)
-     }
+--undent :: Parsec s u ()
+undent :: Parsec (Layout String Identity) u ()
+undent = undent'
+--  do { SourcePos cs line col (p:ps) <- getPosition
+--     ; setPosition (SourcePos cs line col ps)
+--     }
 
 --align :: Parsec [Char] u a -> Parsec [Char] u b -> Parsec [Char] u [a]
 align p stop =
-  do { x <- p
+  do { x <- p --; error "got DECL"
      ; whiteSpace
      ; (do { try layoutSep; xs <- align p stop; return (x:xs)}) <|>
        (do { try layoutEnd; stop; return [x]}) <|>
@@ -172,25 +164,19 @@ oper = do { c <- (P.opStart tokenDef')
 
 reservedOp' op = reservedOp op >> return op
 
-undefined1 = error "undefined1"
-undefined2 = error "undefined2"
-undefined3 = error "undefined3"
 undefined4 = error "undefined4"
-undefined5 = error "undefined5"
 
 parseFromFile :: Parsec (Layout String Identity) u a -> Pos.SourceName -> IO (Either ParseError a)
 parseFromFile p fname
     = do { input <- readFile fname
-         ; return (runP (unitState' p) () fname $ intoLayout input)
+         ; return (runP (unitState p) () fname $ intoLayout input)
          }
 
 parseFromHandle :: Parsec (Layout String Identity) u a -> Pos.SourceName -> Handle -> IO (Either ParseError a)
 parseFromHandle p fname h
     = do { input <- hGetContents h
-         ; return (runP (unitState' p) () fname $ intoLayout input)
+         ; return (runP (unitState p) () fname $ intoLayout input)
          }
---parseFromHandle :: t -> u -> u -> IO (Either String b)
---parseFromHandle = undefined
 
 prefixIdentifier c =
     lexeme $ try $
@@ -201,7 +187,6 @@ prefixIdentifier c =
            else return name
        }
 
-
 type Parser a = Parsec (Layout String Identity) () a
 
 
@@ -210,10 +195,13 @@ type Parser a = Parsec (Layout String Identity) () a
 data Layout s m where
   Indent :: (Monad m, Stream s m Char) => ![Int] -> !Int -> !Bool -> s -> Layout s m
 
+--deriving instance Show (Layout String m)
+
 class Stream s m t => LayoutStream s m t l where
   virtualSep :: ParsecT (l s m) u m ()
   virtualEnd :: ParsecT (l s m) u m ()
   indent' :: ParsecT (l s m) u m ()
+  undent' :: ParsecT (l s m) u m ()
   intoLayout :: s -> l s m
   outofLayout :: l s m -> s
 
@@ -222,9 +210,11 @@ instance Stream s m Char => LayoutStream s m Char Layout where
   outofLayout (Indent _ _ _ s) = s
   indent' = do (Indent tabs col c'ed s) <- getInput
                setInput $ Indent (col:tabs) col True s
+  undent' = do (Indent (_:tabs) col c'ed s) <- getInput
+               setInput $ Indent tabs col False s
   virtualSep = do (Indent tabs@(tab:_) col False s) <- getInput
                   guard $ col == tab
-                  setInput $ Indent tabs col True s
+                  traceShow ("virtualSep", tabs, col) $ setInput $ Indent tabs col True s
   virtualEnd = do (Indent (tab:out) col False s) <- getInput
                   guard $ col < tab
                   setInput $ Indent out col False s
@@ -238,7 +228,7 @@ instance (Monad m, Stream s m Char) => Stream (Layout s m) m Char where
                                          Just ('\n', s') -> return $ Just ('\n', Indent tabs 0 False s')
                                          Just (t, s') -> case (tabs, c'ed) of
                                                          ((tab:_), False) | t /= ' ' && col == tab -> return Nothing
-                                                         _ -> return $ Just (t, Indent tabs (col + 1) False s')
+                                                         _ -> traceShow (tabs, col+1, c'ed, t, unsafeCoerce s' :: String) (return $ Just (t, Indent tabs (col + 1) False s'))
 
 {-
  a = let
@@ -254,7 +244,7 @@ inp :: Layout String Identity
 inp = intoLayout " a = let\n  b = id\n   1\n  c = 2 in b\n f = 3\n \n 3 = let in 6\n"
 
 lexeme' p = do good <- p ; many (oneOf " \n\t"); return good
-identifier' = lexeme' (many1 $ oneOf "abcdi")
+identifier' = identifier -- lexeme' (many1 $ oneOf "abcdi")
 num' = lexeme' (many1 $ oneOf ['0' .. '9'])
 prgm  = space >> identifier' >> lexeme' (string "= let") >> indent' >> virtualSep >> identifier'
 prgm' = space >> identifier' >> lexeme' (string "= let") >> indent'
