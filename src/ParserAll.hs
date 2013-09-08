13602df23c0111978ed39f75e8a1889ddfa84fb3
Gabor Greif <ggreif@users.noreply.github.com>
2013-09-08 15:58:06 +0000

    clean up

--- src/ParserAll.hs
+++ src/ParserAll.hs
@@ -215,10 +215,10 @@ instance (Monad m, Stream s m Char) => Stream (Layout s m) m Char where
                                          Just ('\t', s') -> let over = col + 8 in return $ Just ('\t', Indent tabs (over - over `mod` 8) False s')
                                          Just ('\n', s') -> return $ Just ('\n', Indent tabs 0 False s')
                                          Just (t, s') -> case (tabs, c'ed) of
-                                                         ([], _) -> justAdvance -- return $ Just (t, Indent tabs (col + 1) False s')
-                                                         _ | otherWhiteSpace s t -> justAdvance -- return $ Just (t, Indent tabs (col + 1) False s')
+                                                         ([], _) -> justAdvance -- deactivated layout
+                                                         _ | otherWhiteSpace s t -> justAdvance
                                                          ((tab:_), False) | t /= ' ' && col <= tab -> return Nothing
-                                                         _ -> justAdvance -- return $ Just (t, Indent tabs (col + 1) False s')
+                                                         _ -> justAdvance
                                             where justAdvance = return $ Just (t, Indent tabs (col + 1) False s')
     where otherWhiteSpace s '-' = True
           otherWhiteSpace _ _ = False
