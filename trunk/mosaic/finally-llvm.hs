4f0c58e3e503f516e0dc7ecbd7f864b3643a5e17
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2014-04-30 13:38:40 +0000

    some progress with monadic Block

--- trunk/mosaic/finally-llvm.hs
+++ trunk/mosaic/finally-llvm.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DataKinds, KindSignatures, MultiParamTypeClasses
-           , GADTs #-}
+           , FlexibleInstances, GADTs #-}
 
 module FinallyLLVM where
 
@@ -24,13 +24,16 @@ class (LLVM repr, Monad m) => Block m (repr :: LLType -> *) where
 
 -- a free-monad like thingy to implement Block
 data TB a where
-  Ret :: LLVM repr => repr a -> TB (repr a)
+  Ret :: a -> TB a
   Bind :: LLVM repr => repr ty' -> (repr ty' -> m (repr ty)) -> TB (repr ty)
 
 instance Monad TB where
   return = Ret
 
-t1 :: (LLVM repr, Block m repr) => m (repr 'Int)
+instance LLVM repr => Block TB repr where
+  instr = return
+
+t1 :: (LLVM repr) => TB (repr 'Int)
 t1 = do
   let i1 = add (cnst 2) (cnst 40)
   return i1
