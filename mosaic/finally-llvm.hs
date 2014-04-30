358f6f06f0047bd7b3c8f65ea3abdb7304764fde
Gabor Greif <ggreif@users.noreply.github.com>
2014-04-30 12:51:10 +0000

    start adding a type system

--- mosaic/finally-llvm.hs
+++ mosaic/finally-llvm.hs
@@ -1,16 +1,25 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, KindSignatures #-}
 
 module FinallyLLVM where
 
-class LLVM repr where
-  cnst :: Int -> repr
-  add :: repr -> repr -> repr
+import GHC.TypeLits
+
+
+data LLType = Int | Label Symbol | Nat `X` LLType
+
+class LLVM (repr :: LLType -> *) where
+  cnst :: Int -> repr 'Int
+  add :: repr ty -> repr ty -> repr ty
   -- (:=) :: repr -> repr -> repr
+  phi :: repr ty -> repr (Label sym) -> repr ty
 
 
 -- TEST
 
-t1 :: LLVM repr => Maybe repr
+t1 :: LLVM repr => Maybe (repr 'Int)
 t1 = do
   let i1 = add (cnst 2) (cnst 40)
   return i1
+
+
+--instance LLVM 
