c8288f5902a5b4af693a5e2aa24e04b4649f5708
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2014-05-02 08:16:51 +0000

    refactor for modularity

--- trunk/mosaic/finally-predicative.hs
+++ trunk/mosaic/finally-predicative.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE DataKinds, KindSignatures #-}
 
---import GHC.TypeLits
-
 data Nat = Z | S Nat
 
 class LC (rep :: Nat -> *) where
-  star :: rep (S (S n))
-  int :: rep (S n)
-  arr :: rep (S n) -> rep (S n) -> rep (S n)
-  cnst :: Int -> rep Z
   var :: rep n
   lam :: rep n -> rep n
   app :: rep n -> rep n -> rep n
+
+class TypedLC (rep :: Nat -> *) where
   annot :: rep n -> rep (S n) -> rep n
+  typeof :: rep n -> rep (S n)
+  arr :: rep (S n) -> rep (S n) -> rep (S n)
+
+class BuiltinLC (rep :: Nat -> *) where
+  star :: rep (S (S n))
+  int :: rep (S n)
+  cnst :: Int -> rep Z
