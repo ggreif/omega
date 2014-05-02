e8d07a606aeb74a6812b39d8487650211c53d391
Gabor Greif <ggreif@users.noreply.github.com>
2014-05-02 08:59:42 +0000

    add tests

--- mosaic/finally-predicative.hs
+++ mosaic/finally-predicative.hs
@@ -16,3 +16,17 @@ class BuiltinLC (rep :: Nat -> *) where
   star :: rep (S (S n))
   int :: rep (S n)
   cnst :: Int -> rep Z
+
+
+-- ## TESTS ##
+
+t1, t2 :: LC rep => rep Z
+t1 = lam var
+t2 = t1 `app` t1
+
+newtype LString (n :: Nat) = L { unL :: String } deriving Show
+
+instance LC LString where
+  var = L "?"
+  lam body = L $ "(\\" ++ unL body ++ ")"
+  app e1 e2 = L $ "(" ++ unL e1 ++ " " ++ unL e2 ++ ")"
