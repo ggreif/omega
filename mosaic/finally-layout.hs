18061463f60ee0c4bb70582b509bdebe86a9dd20
Gabor Greif <ggreif@users.noreply.github.com>
2014-07-26 18:39:59 +0000

    WIP: emulate something monad-like (nothing works)

--- mosaic/finally-layout.hs
+++ mosaic/finally-layout.hs
@@ -1,20 +1,25 @@
 {-# LANGUAGE DataKinds, KindSignatures, TypeOperators
            , PolyKinds, GADTs, MultiParamTypeClasses
-           , FlexibleContexts, ScopedTypeVariables #-}
+           , FlexibleContexts, ScopedTypeVariables
+           , RebindableSyntax #-}
 
 
 import GHC.TypeLits
 import Data.Proxy
+import Data.String
 
 data Size s where Size :: KnownNat s => Size s
 data Name n where Name :: KnownSymbol n => Name n
---type Name = KnownSymbol
 
-data L = Symbol `Reg` Nat
+data L = Symbol `Reg` Nat | L `After` L
 
-class Monad m => Layout m (proxy :: L -> *) where
+class {-Monad m => -}Layout m (proxy :: L -> *) where
   reg :: Name n -> Size s -> m (proxy (n `Reg` s))
   reg' :: (KnownSymbol n, KnownNat s) => m (proxy (n `Reg` s))
+  return :: a -> m a
+  (>>) :: m (proxy l) -> m (proxy l') ->  m (proxy (l `After` l'))
+  (>>=) :: m (proxy l) -> (proxy l -> m (proxy l')) ->  m (proxy (l `After` l'))
+  fail :: String -> m a
 
 t1 :: Layout m Proxy => m (Proxy ("FOO" `Reg` 4), Proxy ("BAR" `Reg` 4))
 t1 = do lens1 <- reg' :: m (Proxy ("FOO" `Reg` 4))
