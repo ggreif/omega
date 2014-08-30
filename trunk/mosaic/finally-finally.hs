8c7c3b453131f8d034aa73e1186240ee8c355bb2
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2014-08-30 12:55:50 +0000

    the going gets tough

--- trunk/mosaic/finally-finally.hs
+++ trunk/mosaic/finally-finally.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE ConstraintKinds, DataKinds, PolyKinds, RankNTypes, ImpredicativeTypes #-}
+{-# LANGUAGE ConstraintKinds, DataKinds, PolyKinds, RankNTypes, ImpredicativeTypes, TypeFamilies
+           , MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
 
 -- To have lenses etc. for finally-tagless form we need a
 -- way to create custom class dictionaries (and possibly
@@ -11,15 +12,24 @@ class Foo' a where
   bar :: Char -> Bool -> a
   quux :: a -> a -> a
 
-class Dict (d :: [*] -> k) where
+
+
+class Dict (d :: [*] -> k) (ms :: [*]) where
+  type First d ms :: *
+  first :: First d ms
 
 --type F = Functor
 -- ERROR?! type Foo = forall a . Foo' a => '[Char -> Bool -> a, a -> a -> a]
-type Foo = '[forall a . Foo' a => Char -> Bool -> a, forall a . Foo' a => a -> a -> a]
+--type Foo = '[forall a . Foo' a => Char -> Bool -> a, forall a . Foo' a => a -> a -> a]
+type Foo a = '[Char -> Bool -> a, a -> a -> a]
 
 
-data F (vs :: [*]) = XX
+data F (vs :: [*]) = Bar Char Bool | Quux (F vs) (F vs)
 class G (vs :: [*]) where
 
-instance Dict F where
-instance Dict G where
+instance Dict F (Foo (F a)) where
+  --type First F Foo = forall a . Foo' a => Char -> Bool -> a
+  type First F (Foo (F a)) = Char -> Bool -> F a
+  first = Bar
+
+--instance Dict G (Foo (G a)) where
