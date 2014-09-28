d0f86ceac8631254b093a045b4fe72eb9c7cc1c9
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2014-09-28 10:35:54 +0000

    Andres' idea to make the result indices independently poymorphic

--- trunk/mosaic/HoleTree.hs
+++ trunk/mosaic/HoleTree.hs
@@ -12,15 +12,9 @@ data HTree :: Nat -> Nat -> * -> * where
     Hole :: HTree (S n) n a
     Fork :: HTree m n a -> HTree n o a -> HTree m o a
 
-fill :: Vec m a -> HTree m n a -> (HTree n n a, Vec n a)
+fill :: Vec m a -> HTree m n a -> (HTree o o a, Vec n a)
 fill xs (Leaf x) = (Leaf x, xs)
 fill (x `Cons` xs) Hole = (Leaf x, xs)
-fill xs (Fork a b) =
-    case fill xs a of
-        (a', xs') -> case fill xs' b of
-                        (b', xs'') -> (_, xs'')
-{-
 fill xs (Fork a b) = (Fork a' b', xs'')
     where (a', xs') = fill xs a
           (b', xs'') = fill xs' b
--}
