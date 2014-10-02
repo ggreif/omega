8c5afc31eb72299c87eacca62bbd9b36b25a2663
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2014-10-02 06:24:09 +0000

    add exit gracefully

--- trunk/mosaic/trainer.hs
+++ trunk/mosaic/trainer.hs
@@ -2,6 +2,7 @@
 
 import Test.QuickCheck
 import System.IO.Unsafe
+import System.Exit
 
 prop_addDigits max a b = a >= 0 && b >= 0 && a <= max && b <= max ==> a + b == leleka a b
 
@@ -9,7 +10,9 @@ leleka :: Int -> Int -> Int
 leleka a b = unsafePerformIO $ do
     putStrLn $ "Was ist die Summe von " ++ show a ++ " und " ++ show b ++ "?"
     str <- getLine
-    return $ read str
+    if null str
+      then exitSuccess
+      else return $ read str
 
 
 main = quickCheck $ prop_addDigits 15
