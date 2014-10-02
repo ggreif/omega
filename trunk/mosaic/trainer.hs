6c5196f17402074cf70b9b4a130125bba9e19ae2
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2014-10-02 06:33:33 +0000

    use exitImmediately

--- trunk/mosaic/trainer.hs
+++ trunk/mosaic/trainer.hs
@@ -3,15 +3,16 @@
 import Test.QuickCheck
 import System.IO.Unsafe
 import System.Exit
+import System.Posix.Process
 
 prop_addDigits max a b = a >= 0 && b >= 0 && a <= max && b <= max ==> a + b == leleka a b
 
 leleka :: Int -> Int -> Int
 leleka a b = unsafePerformIO $ do
-    putStrLn $ "Was ist die Summe von " ++ show a ++ " und " ++ show b ++ "?"
+    putStrLn $ "Was ist die Summe von " ++ show a ++ " und " ++ show b ++ " ?"
     str <- getLine
     if null str
-      then exitSuccess
+      then exitImmediately ExitSuccess >> return 0
       else return $ read str
 
 
