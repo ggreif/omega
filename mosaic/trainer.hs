cf57a535e6926797b66d9399fd59e91fca009148
Gabor Greif <ggreif@users.noreply.github.com>
2014-10-02 06:02:56 +0000

    simpleminded trainer for addition

---
+++ mosaic/trainer.hs
@@ -0,0 +1,13 @@
+import Test.QuickCheck
+import System.IO.Unsafe
+
+prop_addDigits max a b = a >= 0 && b >= 0 && a <= max && b <= max ==> a + b == leleka a b
+
+leleka :: Int -> Int -> Int
+leleka a b = unsafePerformIO $ do
+    putStrLn $ "Was ist die Summe von " ++ show a ++ " und " ++ show b ++ "?"
+    str <- getLine
+    return $ read str
+
+
+main = quickCheck $ prop_addDigits 15
