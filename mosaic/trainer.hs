506e0c4eb2d63ef6456684819be18ade55f230f5
Gabor Greif <ggreif@users.noreply.github.com>
2014-10-03 18:07:23 +0000

    add retry possibility; untabify

--- mosaic/trainer.hs
+++ mosaic/trainer.hs
@@ -12,17 +12,23 @@ prop_addDigits max ((`rem` max) -> a) ((`rem` max) -> b) = a >= 0 && b >= 0 && a
 leleka :: Int -> Int -> Int
 leleka a b = unsafePerformIO $ do
     putStrLn $ "Was ist die Summe von " ++ show a ++ " und " ++ show b ++ " ?"
-    str <- getLine
-    correct str
-  where correct "" = do putStrLn "Beenden? (Wenn nicht, die Zahl eingeben.)"
-  		     	str <- getLine
-			if zahl str
-			  then return $ read str
-			  else exitImmediately ExitSuccess >> return 0
-	correct a | zahl a = return $ read a
-	correct x = do putStrLn $ "Das ist keine Zahl: " ++ x
-		       putStrLn $ "Was ist " ++ show a ++ " + " ++ show b ++ " ?"
-		       getLine >>= correct
-	zahl x = not (null x) && all (`elem` ['0'..'9']) x
+    getLine >>= correct 1
+  where correct :: Int -> String -> IO Int
+        correct n "" = do putStrLn "Beenden? (Wenn nicht, die Zahl eingeben.)"
+                          str <- getLine
+                          if zahl str
+                            then sicher n (read str)
+                            else exitImmediately ExitSuccess >> return 0
+        correct n a | zahl a = sicher n (read a)
+        correct n x = do putStrLn $ "Das ist keine Zahl: " ++ x
+                         putStrLn $ "Was ist " ++ show a ++ " + " ++ show b ++ " ?"
+                         getLine >>= correct n
+        sicher :: Int -> Int -> IO Int
+        sicher 0 antw = return antw
+        sicher n antw = if antw == a + b
+                           then return antw
+                           else do putStrLn $ "Bist du sicher? Probier's nochmal:"
+                                   getLine >>= correct (n - 1)
+        zahl x = not (null x) && all (`elem` ['0'..'9']) x
 
 main = quickCheck $ prop_addDigits 15
