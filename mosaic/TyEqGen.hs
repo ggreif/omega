ff5e582bb84b93511bf85c01a1e276c4474f5366
Gabor Greif <ggreif@users.noreply.github.com>
2014-10-18 11:58:29 +0000

    progress on sameRep
    things look good

--- mosaic/TyEqGen.hs
+++ mosaic/TyEqGen.hs
@@ -6,8 +6,13 @@ import Data.Type.Equality
 import GHC.Generics
 import Unsafe.Coerce (unsafeCoerce)
 
-sameRep :: (Generic a, Generic b) => Rep a x -> Rep b x -> Maybe (a :~: b)
-sameRep = undefined
+--sameRep :: (Generic a, Generic b) => Rep a x -> Rep b x -> Maybe (a :~: b)
+
+sameRep :: (Datatype d, Datatype d', SameStructure f f') => D1 d f a -> D1 d' f' a -> Maybe (D1 d f :~: D1 d' f')
+--sameRep = undefined
+sameRep l@(M1 l') r@(M1 r') = do Refl <- sameDatatype l r
+	      	        	 Refl <- sameStructure l' r'
+				 return Refl
 
 
 nameAndMod :: Datatype d => D1 d f a -> (String, String)
@@ -20,8 +25,14 @@ sameDatatype _ _ = Nothing
 class SameStructure f f' where
   sameStructure :: f a -> f' a -> Maybe (f :~: f')
 
+both :: (f :+: g) a -> (f a, g a)
+both = const (undefined, undefined)
+
 instance (SameStructure f f', SameStructure g g') => SameStructure (f :+: g) (f' :+: g') where
-  
+  sameStructure (both -> (x, y)) (both -> (x', y')) = do Refl <- x `sameStructure` x'
+  		      	     	       	       	      	 Refl <- y `sameStructure` y'
+							 return Refl
+
 instance (Constructor c, Constructor c') => SameStructure (C1 c f) (C1 c' f') where
   sameStructure l r = do Refl <- sameConstructor l r; return Refl
 
