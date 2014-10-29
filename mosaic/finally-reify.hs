4f3dbcb3dac3aa8e85580f902e3e834a451f62d6
Gabor Greif <ggreif@users.noreply.github.com>
2014-10-29 02:37:46 +0000

    this shows an error, unfortunately :-(

--- mosaic/finally-reify.hs
+++ mosaic/finally-reify.hs
@@ -1,9 +1,26 @@
-{-# LANGUAGE FunctionalDependencies, MultiParamTypeClasses, TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances, FunctionalDependencies, MultiParamTypeClasses, TypeFamilies #-}
 
-import Data.Monoid
+import Data.Monoid hiding ((<>))
 
 class (Monoid a, Monad m, a ~ m i) => Bag m a i | a -> m i where
-  into :: i -> Bag m a i
+  into :: i -> m i
+  (<>) :: a -> a -> a
+  (<>) = mappend
 
 --class Monad (Bag m a) where
 --  return = into
+
+instance Monoid (IO ()) where
+  mempty = return ()
+  mappend = (>>)
+
+instance Bag IO (IO ()) () where
+  into = return
+
+test1 :: Bag m a i => a
+test1 = mempty
+
+test2 :: Bag m a i => a
+test2 = do a <- putStrLn "Hey"
+           b <- into a
+           putStrLn "You"
