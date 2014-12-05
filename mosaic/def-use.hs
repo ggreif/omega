e5c2fc7b38fe7d90fb7e4e492006944a13193524
Gabor Greif <ggreif@users.noreply.github.com>
2014-12-05 19:06:08 +0000

    start warming up

--- mosaic/def-use.hs
+++ mosaic/def-use.hs
@@ -1,25 +1,34 @@
 {-# LANGUAGE DataKinds, KindSignatures, RankNTypes, StandaloneDeriving, GADTs, TypeOperators, FlexibleInstances, ViewPatterns, UndecidableInstances #-}
 
-import qualified GHC.TypeLits as L
-import GHC.TypeLits (type (+))
+--import qualified GHC.TypeLits as L
+--import GHC.TypeLits (type (+))
 
 
 data Nat' = Z' | S' Nat'
 
 data Nat :: Nat' -> * where
-  Z :: Nat 0
-  S :: Nat n -> Nat (1+n)
+  Z :: Nat Z'
+  S :: Nat n -> Nat (S' n)
 
-class KnownNat (n :: L.Nat) where
+class KnownNat (n :: Nat') where
   theNat :: Nat n
 
-instance KnownNat 0 where
+instance KnownNat Z' where
   theNat = Z
 
-instance (KnownNat n, m ~ (1+n)) => KnownNat m where
+instance KnownNat n => KnownNat (S' n) where
   theNat = S theNat
 
 deriving instance Show (Nat n)
 
 -- Def-Use markers
 
+----------- def     use
+data Lam :: Nat' -> Nat' -> * where
+  L :: (Lam a b -> Lam a b) -> Lam a b
+  (:&) :: Lam a b -> Lam a b -> Lam a b
+
+
+
+test :: Lam Z' Z'
+test = L (\a -> a :& a)
