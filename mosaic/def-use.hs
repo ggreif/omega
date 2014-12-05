2daa066efdd441d095576a48cb04f29d7aa37517
Gabor Greif <ggreif@users.noreply.github.com>
2014-12-05 21:22:15 +0000

    use Dummy for show
    test more expressions

--- mosaic/def-use.hs
+++ mosaic/def-use.hs
@@ -27,12 +27,15 @@ type PlusTen n = PlusFive (PlusFive n)
 
 ----------- def     use
 data Lam :: Nat' -> Nat' -> * where
-  L :: (KnownNat d, KnownNat u) => ((forall u . KnownNat u => Lam (S' d) u) -> Lam d u) -> Lam d u
+  Dummy :: (KnownNat d, KnownNat u) => Lam d u -- only for Show!
+  L :: (KnownNat d, KnownNat u) => ((forall u . KnownNat u => Lam (S' d) u) -> Lam (S' d) u) -> Lam d u
   (:&) :: (KnownNat d, KnownNat d', KnownNat d'', KnownNat u) => (Lam d' (PlusFour d)) -> (Lam d'' (PlusFive d)) -> Lam d u
 
 instance Show (Lam def use) where
-  show lam@(L f) = "(\\" ++ show (f undefined) ++ ")" ++ duStr lam
-  show all@(f :& a) = "(" ++ duStr f ++ "&" ++ duStr a ++ ")" ++ duStr all
+  show lam@(L f) = "(\\" ++ show (f Dummy) ++ ")" ++ duStr lam
+  --show all@(f :& a) = "(" ++ duStr f ++ " & " ++ duStr a ++ ")" ++ duStr all
+  show all@(f :& a) = "(" ++ show f ++ " & " ++ show a ++ ")" ++ duStr all
+  show d@Dummy = duStr d
 
 duStr :: forall def use . (KnownNat def, KnownNat use) => Lam def use -> String
 duStr l = "d" ++ nat2str (def l) ++ "u" ++ nat2str (use l)
@@ -44,9 +47,13 @@ use _ = theNat
 
 nat2int :: Nat n -> Int
 nat2int Z = 0
-nat2int (S (nat2int -> n)) = n + 1
+nat2int (S n) = nat2int n + 1
 
 nat2str = show . nat2int
 
 test :: Lam Z' (PlusTen Z')
 test = L (\a -> a :& a)
+test' :: Lam Z' Z'
+test' = L $ \x->x
+test'' :: Lam Z' Z'
+test'' = L $ \x->L $ \x->x
