58d1c399e72bb46ceae7492a70aa7ff2c0006acb
Gabor Greif <ggreif@users.noreply.github.com>
2014-12-05 20:24:57 +0000

    it WORKS!!!!
    *Main> test
    (\(d1u4&d1u5)d0u0)d0u0

--- mosaic/def-use.hs
+++ mosaic/def-use.hs
@@ -30,10 +30,24 @@ type PlusFive n = S' (S' (S' (S' (S' n))))
 
 ----------- def     use
 data Lam :: Nat' -> Nat' -> * where
-  L :: ((forall b . Lam a b) -> Lam a b') -> Lam a b'
-  (:&) :: (Lam a' (S' a)) -> (Lam a' a) -> Lam a b
+  L :: (KnownNat d, KnownNat u') => ((forall u . KnownNat u => Lam (S' d) u) -> Lam d u') -> Lam d u'
+  (:&) :: (KnownNat d, KnownNat d', KnownNat d'', KnownNat u) => (Lam d' (PlusFour d)) -> (Lam d'' (PlusFive d)) -> Lam d u
 
+instance Show (Lam def use) where
+  show lam@(L f) = "(\\"++show (f undefined)++")"++duStr lam
+  show all@(f :& a) = "("++duStr f++"&"++duStr a++")"++duStr all
 
+duStr :: forall def use . (KnownNat def, KnownNat use) => Lam def use -> String
+duStr l = "d" ++ show (nat2int $ def l) ++ "u" ++ show (nat2int $ use l)
+
+def :: KnownNat def => Lam def use -> Nat def
+def _ = theNat
+use :: KnownNat use => Lam def use -> Nat use
+use _ = theNat
+
+nat2int :: Nat n -> Int
+nat2int Z = 0
+nat2int (S (nat2int -> n)) = n + 1
 
 test :: Lam Z' Z'
 test = L (\a -> a :& a)
