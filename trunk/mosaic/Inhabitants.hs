e0b6431bea49a1f3d2a52ab3c376ff72a73c1511
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2015-02-06 21:57:21 +0000

    pretty natural syntax for 'drawing inhabitants out of an open type' or 'rabbits out of a hat' :-)

--- trunk/mosaic/Inhabitants.hs
+++ trunk/mosaic/Inhabitants.hs
@@ -1,18 +1,25 @@
-{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE DataKinds, GADTs, RebindableSyntax #-}
 
 import qualified Prelude as P
 
 
-data Lam = App Lam Lam | Inh (Lam -> Lam)
+data Nat' = Z' | S' Nat'
+
+--data Nat = 
+
+data Lam l = App (Lam l) (Lam l) | Inh (Lam (S' l)) (Lam l -> Lam l)
+
+star :: Lam (S' Z')
+star = P.undefined
 
 
 
-(>>=) :: () -> (Lam -> Lam) -> Lam
-(>>=) () = Inh
+(>>=) :: Lam (S' l) -> (Lam l -> Lam l) -> Lam l
+(>>=) = Inh
 
 return a = a
 fail = P.error
 
-main' = do i <- ()
+main' = do i <- star
            i
            
