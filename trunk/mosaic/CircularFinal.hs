2926a745a304da1b7a9c9b5de723284be4ba3241
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2015-03-07 11:23:13 +0000

    finalize implementation from paper

--- trunk/mosaic/CircularFinal.hs
+++ trunk/mosaic/CircularFinal.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, UndecidableInstances
-           , FlexibleInstances, FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 import Prelude hiding (max)
+import qualified Prelude as Prelude (max)
 
 -- see: Axelsson, Klaessen: Using Circular Programs for Higher-Order Syntax
 -- ICFP 2013
@@ -13,26 +13,43 @@ class LC p where
 -- first order interpretation
 
 data Lam name = Var name | App (Lam name) (Lam name) | Lam name (Lam name)
+  deriving Show
 
 -- join monoid
 
---class LC (p t) => Join p t | p -> t where
---class LC (lc t) => Join lc t where
 class Join t where
   bot :: t
   prime :: t -> t
   (|-|) :: t -> t -> t
-  --max :: lc t -> t
-  max :: LC (lc t) => lc t -> t
 
---instance Join (Lam name) name => LC (Lam name) where
---instance Join Lam name => LC (Lam name) where
+class (Join t, LC (lc t)) => Max lc t where
+  max :: lc t -> t
+
 instance Join name => LC (Lam name) where
   app = App
   lam f = Lam name body
     where body = f (Var name)
           name = prime (max body)
 
---instance Join (Lam name) name
---instance Join Lam name
-instance Join name
+instance Join Int where
+  bot = 0
+  prime = (+1)
+  (|-|) = Prelude.max
+
+instance Join name => Max Lam name where
+  max (Var _) = bot
+  max (f `App` a) = max f |-| max a
+  max (Lam name _) = name
+
+
+t1' :: LC p => p
+t1' = lam (\a -> a `app` a)
+
+t1 :: Lam Int
+t1 = t1'
+
+t2' :: LC p => p
+t2' = lam (\a -> lam (\b -> a `app` b))
+
+t2 :: Lam Int
+t2 = t2'
