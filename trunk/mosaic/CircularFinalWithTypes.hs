caf7d935643c87347aa187c8b40328fff75a2afe
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2015-03-08 02:29:17 +0000

    make the second coordinate a lambda term (type-level)  for now just wrap it with Var

--- trunk/mosaic/CircularFinalWithTypes.hs
+++ trunk/mosaic/CircularFinalWithTypes.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, FlexibleContexts, TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 import Prelude hiding (max)
 import qualified Prelude as Prelude (max)
 
@@ -29,7 +29,7 @@ class Max lc t where
   max :: lc t -> t
 
 --instance Join name => LC (Lam (name, Int)) where
-instance LC (Lam (Int, Int)) where
+instance LC (Lam (Int, Lam Int)) where
   app = App
   lam f = Lam name body
     where body = f (Var name)
@@ -39,14 +39,6 @@ instance Join Int where
   bot = 0
   prime = (+1)
   (|-|) = Prelude.max
-
-{-
-instance Join name => Max Lam name where
-  max (Var _) = bot
-  max (f `App` a) = max f |-| max a
-  max (a `Cho` b) = max a |-| max b
-  max (Lam name _) = name
--}
 
 t1' :: LC p => p
 t1' = lam (\a -> a `app` a)
@@ -63,7 +55,7 @@ t2' = lam (\a -> lam (\b -> a `app` b))
 t3' :: Indet p => p
 t3' = lam (\a -> lam (\b -> a ~~ b))
 
-t3 :: Lam (Int, Int)
+t3 :: Lam (Int, Lam Int)
 t3 = t3'
 
 -- ########### can we use a similar trick for type inference? ###########
@@ -75,27 +67,22 @@ class LC p => Indet p where
 
 
 --instance Join name => Indet (Lam (name, Int)) where
-instance Indet (Lam (Int, Int)) where
+instance Indet (Lam (Int, Lam Int)) where
   (~~) = Cho
 
-instance Join name => Join (name, Int) where
-  bot = (bot, 42)
+instance Join name => Join (name, Lam Int) where
+  bot = (bot, Var 42)
   prime (p, x) = (prime p, x)
   (a, x) |-| (b, y) = (a |-| b, x)
 
 --instance (name ~ Int, Join name) => Max Lam (name, Int) where
-instance Max Lam (Int, Int) where
-  max v@(Var n) = (bot, fst n)
-  max (f `App` a) = (fst (max f) |-| fst (max a), 88)
-  max (a `Cho` b) = (fst ma |-| fst mb, snd ma `Prelude.max` snd mb)
+instance Max Lam (Int, Lam Int) where
+  max v@(Var n) = (bot, Var $ fst n)
+  max (f `App` a) = (fst (max f) |-| fst (max a), Var 88)
+  max (a `Cho` b) = (fst ma |-| fst mb, snd ma `uni` snd mb)
     where ma = max a
           mb = max b
+          uni :: Lam Int -> Lam Int -> Lam Int
+          Var l `uni` Var r = Var $ l `Prelude.max` r
   max (Lam (name, ty) _) = (name, ty)
 
-{-
-instance Join name => Max Lam (name, Int) where
-  max v@(Var _) = (bot, 77)
-  max (f `App` a) = (fst (max f) |-| fst (max a), 88)
-  max (a `Cho` b) = (fst (max a) |-| fst (max b), 999)
-  max (Lam (name, ty) _) = (name, ty)
--}
