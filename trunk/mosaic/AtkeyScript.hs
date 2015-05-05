9633cf577c3b56ba576c873ef748a1e147c6bb1d
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2015-05-05 22:01:17 +0000

    WIP: transitioning to typeclass `TypeChecker`
    No instance for (TypeChecker ([Type] -> Maybe Type))

--- trunk/mosaic/AtkeyScript.hs
+++ trunk/mosaic/AtkeyScript.hs
@@ -1,6 +1,10 @@
 -- this mosaic explores the way laid out by Robert Atkey in
 --  http://bentnib.org/posts/2015-04-19-algebraic-approach-typechecking-and-elaboration.html
 
+-- The Algebraic Theory of Typechecking
+--  around slide 30
+--   (for previous slides consult revision history)
+
 {-# LANGUAGE TypeOperators #-}
 
 import Control.Monad
@@ -9,42 +13,49 @@ data Type = A | B | C | Type :-> Type deriving (Eq, Show)
 
 infixr 9 :->
 
-type TypeChecker = [Type] -> Maybe Type
+type TypeChecker' = [Type] -> Maybe Type
 
 -- a TypeChecker is basically something that maybe has a type in a context
---   (e.g. a Term)
+--   (e.g. corresponding to a Term)
 
-var :: Int -> TypeChecker
-var i = return . (!!i)
+class TypeChecker a where
 
-lam :: Type -> TypeChecker -> TypeChecker
-lam ty tc ctx = do tbody <- tc $ ty : ctx
-                   return $ ty :-> tbody
+  var :: Int -> a
+-- var i = return . (!!i)
 
-app :: TypeChecker -> TypeChecker -> TypeChecker
-app cf ca ctx = do (ta :-> tr) <- cf ctx
-                   ta' <- ca ctx
-                   guard $ ta == ta'
-                   return tr
+  lam :: Type -> a -> a
+-- lam ty tc ctx = do tbody <- tc $ ty : ctx
+--                    return $ ty :-> tbody
+
+  app :: a -> a -> a
+--app cf ca ctx = do (ta :-> tr) <- cf ctx
+--                   ta' <- ca ctx
+--                   guard $ ta == ta'
+--                   return tr
+
+  failure :: a
+  have :: Int -> Type -> a -> a
+  hasType :: Type -> a -> a
+
 
 data Term = Var Int | Lam Type Term | Term `App` Term deriving Show
 
-typeCheck :: Term -> TypeChecker
+typeCheck :: Term -> TypeChecker'
 typeCheck (Var i) = var i
 typeCheck (Lam ty tm) = lam ty $ typeCheck tm
 typeCheck (f `App` a) = typeCheck f `app` typeCheck a
 
 
 -- More bits
-failure :: TypeChecker
-failure = const Nothing
+failure' :: TypeChecker'
+failure' = const Nothing
 
-have :: Int -> Type -> TypeChecker -> TypeChecker
-have i ty tc ctx = do ty' <- var i ctx
-                      guard $ ty == ty'
-                      tc ctx
-
-hasType :: Type -> TypeChecker -> TypeChecker
-hasType ty tc ctx = do ty' <- tc ctx
+have' :: Int -> Type -> TypeChecker' -> TypeChecker'
+have' i ty tc ctx = do ty' <- var i ctx
                        guard $ ty == ty'
-                       return ty
+                       tc ctx
+
+hasType' :: Type -> TypeChecker' -> TypeChecker'
+hasType' ty tc ctx = do ty' <- tc ctx
+                        guard $ ty == ty'
+                        return ty
