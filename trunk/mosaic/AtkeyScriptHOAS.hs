f288086e20a057cb9b1e798faab0dff56505cb3d
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2015-05-19 22:19:07 +0000

    walk the HOAS road

---
+++ trunk/mosaic/AtkeyScriptHOAS.hs
@@ -0,0 +1,55 @@
+-- this mosaic explores the way laid out by Robert Atkey in
+--  http://bentnib.org/posts/2015-04-19-algebraic-approach-typechecking-and-elaboration.html
+
+-- The Algebraic Theory of Typechecking
+--  around slide 30
+--   (for previous slides consult revision history)
+
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, TypeOperators #-}
+
+import Control.Monad
+
+data Type = A | B | C | Type :-> Type deriving (Eq, Show)
+
+infixr 9 :->
+
+type TypeChecker' = [Type] -> Maybe Type
+
+-- a TypeChecker is basically something that maybe has a type in a context
+--   (e.g. corresponding to a Term)
+
+class TypeChecker a where
+  var :: Int -> a
+  lam :: Type -> a -> a
+  app :: a -> a -> a
+  failure :: a
+  have :: Int -> Type -> a -> a
+  hasType :: Type -> a -> a
+
+instance TypeChecker TypeChecker' where
+  var i = return . (!!i)
+
+  lam ty tc ctx = do tbody <- tc $ ty : ctx
+                     return $ ty :-> tbody
+
+  app cf ca ctx = do (ta :-> tr) <- cf ctx
+                     ta' <- ca ctx
+                     guard $ ta == ta'
+                     return tr
+
+  failure = const Nothing
+  have i ty tc ctx = do ty' <- var i ctx
+                        guard $ ty == ty'
+                        tc ctx
+  hasType ty tc ctx = do ty' <- tc ctx
+                         guard $ ty == ty'
+                         return ty
+
+
+data Term = Var Int | Lam Type Term | Term `App` Term deriving Show
+
+typeCheck :: Term -> TypeChecker'
+typeCheck (Var i) = var i
+typeCheck (Lam ty tm) = lam ty $ typeCheck tm
+typeCheck (f `App` a) = typeCheck f `app` typeCheck a
+
