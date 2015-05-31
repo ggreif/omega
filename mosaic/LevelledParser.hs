3973a7bf8f8d9a690b2accc0456ab038b5cc7f4c
Gabor Greif <ggreif@users.noreply.github.com>
2015-05-31 07:52:25 +0000

    sketch how this could work

---
+++ mosaic/LevelledParser.hs
@@ -0,0 +1,37 @@
+
+{-# LANGUAGE DataKinds, KindSignatures, GADTs, ScopedTypeVariables #-}
+-- import Text.Parsec
+
+--import Kinds.TypeMachinery
+import Control.Applicative
+
+data Nat = Z | S Nat
+
+data Nat' :: Nat -> * where
+  Z' :: Nat' Z
+  S' :: Nat' n -> Nat' (S n)
+
+class P (interp :: Nat -> * -> *) where
+  star :: interp s ()
+  reserved :: String -> interp s ()
+  operator :: String -> interp s ()
+  identifier :: interp s String
+  constructor :: interp s String
+  ascend :: interp (S s) a -> interp s a
+  descend :: interp s a -> interp (S s) a
+
+signature :: (P interp, Alternative (interp s)) => interp s ()
+signature = star <|> star
+
+dataDefinition :: forall interp s . (P interp, Monad (interp (S s)), Alternative (interp s), Alternative (interp ('S s))) => interp (S s) (String, (), [String])
+dataDefinition = do reserved "data"
+                    name <- constructor
+                    operator "::"
+                    sig <- signature
+                    reserved "where"
+                    alts <- descend $ many alt
+                    return (name, sig, alts)
+    where alt = constructor -- :: interp s String
+
+
+
