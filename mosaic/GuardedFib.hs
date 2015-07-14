6beaa56f04043c55134eefa80664128fa62ffcea
Gabor Greif <ggreif@users.noreply.github.com>
2015-07-14 11:30:10 +0000

    explain the preblem and annotate the types

--- mosaic/GuardedFib.hs
+++ mosaic/GuardedFib.hs
@@ -1,5 +1,11 @@
 {-# LANGUAGE ViewPatterns #-}
 
+{- The spirit of guarded recursion is that we can only
+   re-enter the recursion with a smaller value.
+   Can we enforce this with view patterns and a type system that
+   tracks effective refinement types?
+-}
+
 import Control.Arrow
 
 data Nat = Z | S Nat deriving Show
@@ -9,11 +15,21 @@ six = S five
 eight = fib six
 twentyOne = fib eight
 
+-- Here comes the challenge: we cannot use the
+-- induction hypothesis on the right hand sides
+-- of our equations, only in the patterns!
+-- (And not on top-level!)
+
 plus m Z = m
 plus m (S (plus m -> sum)) = S sum
-
+---         |
+---         +--> :: Nat -> (S effective ° Nat) -> Nat
 
 fib :: Nat -> Nat
 fib zero@Z = zero
 fib one@(S Z) = one
 fib (S (fib&&&id -> (f, S (fib -> g)))) = f `plus` g
+---      |                  |
+---      |                  +--> :: (S (S effective) ° Nat) -> Nat
+---      |
+---      +--> :: (S effective ° Nat) -> Nat
