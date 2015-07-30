332c889d70ae8166e7c572f3bb49dede36ae8479
Gabor Greif <ggreif@users.noreply.github.com>
2015-07-30 17:29:14 +0000

    looks like I can use the refinement syntax in quotations too

--- mosaic/OmegaQuote.hs
+++ mosaic/OmegaQuote.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds, KindSignatures, QuasiQuotes, GADTs,
              StandaloneDeriving, UnicodeSyntax #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies, TypeOperators, PolyKinds #-}
 
 import OmegaParser
 
@@ -26,3 +27,15 @@ c = [ω|33|]
 
 c1 = [e|33|]
 c2 = [e||id 33||]
+
+type family (narrow :: k) ° (wide :: *)
+infix 0 °
+
+stuff = [d| data Nat = Z | S Nat |]
+data Nat = Z | S Nat
+refinementPlus =
+  [d| plus :: (a ° Nat) -> (b ° Nat) -> (a `plus` b ° Nat)
+      Z `plus` n = n
+      (S m) `plus` n = S (m `plus` n)
+    |]
+
