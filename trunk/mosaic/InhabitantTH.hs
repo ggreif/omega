92c051b8e22b24538d9ac379a7700a3fb08b1d0f
ggreif <ggreif@bc6a98a5-27c3-9af1-fdd8-01175c2dd28c>
2015-08-01 17:21:54 +0000

    create nameable postulates

--- trunk/mosaic/InhabitantTH.hs
+++ trunk/mosaic/InhabitantTH.hs
@@ -1,15 +1,35 @@
+{-# LANGUAGE KindSignatures, GADTs #-}
+
 module InhabitantTH where
 
 import Inhabitants
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 import Language.Haskell.TH.Lib
-import Language.Haskell.TH.Syntax (runQ)
+--import Language.Haskell.TH.Syntax (runQ)
+
+
+class Inhabitable ent => Postulatable ent where
+  postulate :: ent o (S' l) -> ent False l -- should be HOAS (like inhabit)
 
 dataRewrite :: DecsQ -> DecsQ
 dataRewrite q = do decs <- q
                    return $ map go decs
   where go :: Dec -> Dec
-        go (DataD ctxt name tyvs cons derivs) = error $ show tyvs
+        go (DataD ctxt name tyvs cons derivs) = let posts :: [(Name, Nameable False (S' Z'))]; posts = map postulateTvs tyvs in error $ "tvs: " ++ concatMap (show . fst) posts
         go a = error $ show a
 
+        postulateTvs :: Postulatable ent => TyVarBndr -> (Name, ent False (S' Z'))
+        postulateTvs (PlainTV name) = (name, postulate starN)
+        postulateTvs (KindedTV name StarT) = (name, postulate starN)
+        postulateTvs (KindedTV name kind) = error $ "Needs reify? " ++ show kind
+
+
+data Nameable (open :: Bool) (lev :: Nat') where
+  Tyvar :: Nameable o (S' l) -> Name -> Nameable False l
+  Anon :: (Name -> Nameable o l) -> Nameable o l
+
+
+instance Inhabitable (Nameable) where
+instance Postulatable (Nameable) where
+  postulate = Anon . Tyvar
