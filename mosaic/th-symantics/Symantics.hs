cf1ab9055ce3e05021b7e66b8090d273efe18534
Gabor Greif <ggreif@gmail.com>
2015-10-11 19:08:12 +0200

    comment out cruft

--- mosaic/th-symantics/Symantics.hs
+++ mosaic/th-symantics/Symantics.hs
@@ -5,13 +5,14 @@ import Language.Haskell.TH.Syntax
 
 -- GOAL: transform (simple) TH splices to the format defined in ../def-use.hs
 
+{-
 oneLamBinder :: Exp -> Exp
 oneLamBinder (LamE [] e) = e -- FIXME: visit e
 oneLamBinder lam@(LamE [p] e) = lam -- FIXME: visit e
 oneLamBinder (LamE (p:ps) e) = LamE [p] (oneLamBinder (LamE ps e))
 
-
 example = runQ (oneLamBinder <$> [|\a -> a|]) >>= print
+-}
 
 e1 = (toMy [] <$> [|\a b -> b a|]) :: Q (E Easy Root)
 
