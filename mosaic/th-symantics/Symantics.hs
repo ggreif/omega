ba55dfa3d26c791b9a4dc8c0f110dd1e60db337d
Gabor Greif <ggreif@gmail.com>
2015-10-11 12:34:29 +0200

    a big step towards adding locations

--- mosaic/th-symantics/Symantics.hs
+++ mosaic/th-symantics/Symantics.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, ViewPatterns #-}
+{-# LANGUAGE TemplateHaskell, ViewPatterns, KindSignatures, DataKinds, RankNTypes, GADTs #-}
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
@@ -13,13 +13,30 @@ oneLamBinder (LamE (p:ps) e) = LamE [p] (oneLamBinder (LamE ps e))
 
 example = runQ (oneLamBinder <$> [|\a b -> a|]) >>= print
 
-e1 = (toMy [] <$> [|\a b -> a b|]) :: Q Easy
+--e1 = (toMy [] <$> [|\a b -> a b|]) :: Q Easy
 
-class Lam repr where
-  lam :: (repr -> repr) -> repr
-  app :: repr -> repr -> repr
+data Lo = Root | Lef Lo | Rig Lo -- | Abs Loc
 
+class Lam (repr :: Lo -> Lo -> *) where
+  lam :: (forall u' . repr (Lef u) u' -> repr d' (Rig u)) -> repr u u
+  app :: repr u' (Lef u) -> repr u'' (Rig u) -> repr u u
 
+data V (repr :: Lo -> Lo -> *) where
+  V :: (forall u' . repr (Lef u) u') -> V repr
+
+--data E (repr :: Lo -> Lo -> *) where
+--  E :: (forall u . repr d u) -> E repr
+data E (repr :: Lo -> Lo -> *) (use :: Lo) where
+  E :: repr d u -> E repr u
+
+--toMy :: Lam repr => [(Name, V repr)] -> Exp -> repr u u
+--toMy :: Lam repr => [(Name, V repr)] -> Exp -> E repr
+toMy :: Lam repr => [(Name, V repr)] -> Exp -> E repr u
+toMy env (VarE (flip lookup env -> Just (V v))) = E v
+toMy env (AppE f a) = case (toMy env f, toMy env a) of (E f', E a') -> E (app f' a')
+   -- where E f' = toMy env f
+
+{-
 toMy :: Lam repr => [(Name, repr)] -> Exp -> repr
 toMy env (VarE (flip lookup env -> Just v)) = v
 toMy env (AppE f a) = app (toMy env f) (toMy env a)
@@ -27,12 +44,13 @@ toMy env (LamE [] e) = toMy env e
 toMy env (LamE [VarP nam] e) = lam $ \v -> toMy ((nam, v) : env) e
 toMy env (LamE (p:ps) e) = toMy env (LamE [p] $ LamE ps e)
 
-
-data Easy = It | App Easy Easy | Lam (Easy -> Easy)
-
 instance Lam Easy where
   lam = Lam
   app = App
+-}
+
+data Easy = It | App Easy Easy | Lam (Easy -> Easy)
+
 
 instance Show Easy where
   show It = "It"
