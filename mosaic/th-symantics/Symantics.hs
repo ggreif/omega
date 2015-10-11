e8ce07bfd4aa9d3b62b5d649b218ac099257b1d4
Gabor Greif <ggreif@gmail.com>
2015-10-11 13:49:15 +0200

    WIP: explora the "E" route, getting rid of "defless" for now...

--- mosaic/th-symantics/Symantics.hs
+++ mosaic/th-symantics/Symantics.hs
@@ -18,9 +18,10 @@ e1 = (toMy [] <$> [|\a b -> b a|]) :: Q (E Easy Root)
 data Lo = Root | Lef Lo | Rig Lo -- | Abs Loc
 
 class Lam (repr :: Lo -> Lo -> *) where
-  lam :: ((forall u' . repr (Lef u) u') -> repr d' (Rig u)) -> repr u u
+  --lam :: ((forall u' . repr (Lef u) u') -> repr d' (Rig u)) -> repr u u
+  lam :: ((forall u' . repr (Lef u) u') -> E repr (Rig u)) -> repr u u
   app :: repr u' (Lef u) -> repr u'' (Rig u) -> repr u u
-  defless :: repr d' u -> repr d u
+  --defless :: repr d' u -> repr d u
 
 data V (repr :: Lo -> Lo -> *) where
   V :: (forall u' . repr (Lef u) u') -> V repr
@@ -36,24 +37,25 @@ toMy env (VarE (flip lookup env -> Just (V v))) = E v
 toMy env (AppE f a) = case (toMy env f, toMy env a) of (E f', E a') -> E (app f' a')
 toMy env (LamE [] e) = toMy env e
 toMy env (LamE [VarP nam] e) = E (lam f)
-   where f :: (forall u' . repr (Lef u) u') -> repr d' (Rig u)
-         f v = case toMy ((nam, V v) : env) e of E e' -> defless e'
+   where f :: (forall u' . repr (Lef u) u') -> E repr (Rig u)
+         f v = toMy ((nam, V v) : env) e
+         --f v = case toMy ((nam, V v) : env) e of E e' -> defless e'
 toMy env (LamE (p:ps) e) = toMy env (LamE [p] $ LamE ps e)
 
 instance Lam Easy where
   lam = Lam
   app = App
-  defless = Some
+  --defless = Some
 
 data Easy :: Lo -> Lo -> * where
   It :: Easy (Lef u) u'
   App :: Easy u' (Lef u) -> Easy u'' (Rig u) -> Easy u u
-  Lam :: ((forall u' . Easy (Lef u) u') -> Easy d' (Rig u)) -> Easy u u
-  Some :: Easy d' u -> Easy d u
+  Lam :: ((forall u' . Easy (Lef u) u') -> E Easy (Rig u)) -> Easy u u
+  --Some :: Easy d' u -> Easy d u
 
 
 instance Show (Easy d u) where
   show It = "It"
   show (App f a) = "(" ++ show f ++ " " ++ show a ++ ")"
   show (Lam f) = "(\\" ++ show It ++ " -> " ++ show (f It) ++ ")"
-  show (Some e) = show e
+  --show (Some e) = show e
