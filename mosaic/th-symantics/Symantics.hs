bc12af892e6c16f7a6e76f9e6e90b2fadc681eb0
Gabor Greif <ggreif@gmail.com>
2015-10-11 18:09:19 +0200

    lam and It now carry locations

--- mosaic/th-symantics/Symantics.hs
+++ mosaic/th-symantics/Symantics.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, ViewPatterns, KindSignatures, DataKinds, RankNTypes, GADTs, ScopedTypeVariables, FlexibleInstances #-}
+{-# LANGUAGE TemplateHaskell, ViewPatterns, KindSignatures, DataKinds, RankNTypes, GADTs, ScopedTypeVariables, FlexibleInstances, StandaloneDeriving #-}
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
@@ -18,17 +18,26 @@ e1 = (toMy [] <$> [|\a b -> b a|]) :: Q (E Easy Root)
 data Lo = Root | Lef Lo | Rig Lo -- | Abs Loc
 
 class Lam (repr :: Lo -> Lo -> *) where
-  --lam :: ((forall u' . repr (Lef u) u') -> repr d' (Rig u)) -> repr u u
-  lam :: ((forall u' . Known u' => repr (Lef u) u') -> E repr (Rig u)) -> repr u u
+  lam :: Known u => ((forall u' . Known u' => repr (Lef u) u') -> E repr (Rig u)) -> repr u u
   app :: repr u' (Lef u) -> repr u'' (Rig u) -> repr u u
-  --defless :: repr d' u -> repr d u
 
 
-class Known (loc :: Lo)
+data Lo' (loc :: Lo) where
+  Root' :: Lo' Root
+  Lef' :: Lo' loc -> Lo' (Lef loc)
+  Rig' :: Lo' loc -> Lo' (Rig loc)
 
-instance Known Root
-instance Known loc => Known (Lef loc)
-instance Known loc => Known (Rig loc)
+deriving instance Show (Lo' loc)
+
+class Known (loc :: Lo) where
+  loc :: Lo' loc
+
+instance Known Root where
+  loc = Root'
+instance Known loc => Known (Lef loc) where
+  loc = Lef' loc
+instance Known loc => Known (Rig loc) where
+  loc = Rig' loc
 
 data V (repr :: Lo -> Lo -> *) where
   V :: (forall u' . Known u' => repr (Lef u) u') -> V repr
@@ -39,30 +48,26 @@ data E (repr :: Lo -> Lo -> *) (use :: Lo) where
 instance Show (E Easy u) where
   show (E e) = show e
 
-toMy :: forall repr u . (Known u, Lam repr) => [(Name, V repr)] -> Exp -> E repr u
+toMy :: (Known u, Lam repr) => [(Name, V repr)] -> Exp -> E repr u
 toMy env (VarE (flip lookup env -> Just (V v))) = E v
 toMy env (AppE f a) = case (toMy env f, toMy env a) of (E f', E a') -> E (app f' a')
 toMy env (LamE [] e) = toMy env e
 toMy env (LamE [VarP nam] e) = E (lam (\v -> toMy ((nam, V v) : env) e))
-   --where f :: (forall u' . repr (Lef u) u') -> E repr (Rig u)
-   --      f v = toMy ((nam, V v) : env) e
-   --      --f v = case toMy ((nam, V v) : env) e of E e' -> defless e'
 toMy env (LamE (p:ps) e) = toMy env (LamE [p] $ LamE ps e)
 
 instance Lam Easy where
   lam = Lam
   app = App
-  --defless = Some
 
 data Easy :: Lo -> Lo -> * where
-  It :: Easy (Lef u) u'
+  It :: Known u => Easy (Lef u) u'
   App :: Easy u' (Lef u) -> Easy u'' (Rig u) -> Easy u u
-  Lam :: ((forall u' . Easy (Lef u) u') -> E Easy (Rig u)) -> Easy u u
-  --Some :: Easy d' u -> Easy d u
+  Lam :: Known u => ((forall u' . Easy (Lef u) u') -> E Easy (Rig u)) -> Easy u u
 
 
 instance Show (Easy d u) where
-  show It = "It"
+  show (It :: Easy d u) = "It(" ++ show (loc :: Lo' d) ++ ")"
   show (App f a) = "(" ++ show f ++ " " ++ show a ++ ")"
-  show (Lam f) = "(\\" ++ show It ++ " -> " ++ show (f It) ++ ")"
-  --show (Some e) = show e
+  show (Lam f :: Easy d u) = "(\\" ++ show it ++ " -> " ++ show (f it) ++ ")"
+      where  it :: Easy (Lef d) u'
+             it = It
