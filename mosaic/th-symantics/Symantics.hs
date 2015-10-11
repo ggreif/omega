beb0e4f1355886517e8049fca22ba5a08f16f9c2
Gabor Greif <ggreif@gmail.com>
2015-10-11 13:39:59 +0200

    WIP: stopgap solution for the existential "def of HOAS rhs" problem
    this is equivalent to the E wrapper.
    
    E repr u == (defless :: repr d' u -> repr d u)
    
    so why have both??

--- mosaic/th-symantics/Symantics.hs
+++ mosaic/th-symantics/Symantics.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, ViewPatterns, KindSignatures, DataKinds, RankNTypes, GADTs, ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell, ViewPatterns, KindSignatures, DataKinds, RankNTypes, GADTs, ScopedTypeVariables, FlexibleInstances #-}
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
@@ -11,15 +11,16 @@ oneLamBinder lam@(LamE [p] e) = lam -- FIXME: visit e
 oneLamBinder (LamE (p:ps) e) = LamE [p] (oneLamBinder (LamE ps e))
 
 
-example = runQ (oneLamBinder <$> [|\a b -> a|]) >>= print
+example = runQ (oneLamBinder <$> [|\a -> a|]) >>= print
 
-e1 = (toMy [] <$> [|\a b -> a b|]) :: Q (E Easy Root)
+e1 = (toMy [] <$> [|\a b -> a|]) :: Q (E Easy Root)
 
 data Lo = Root | Lef Lo | Rig Lo -- | Abs Loc
 
 class Lam (repr :: Lo -> Lo -> *) where
   lam :: ((forall u' . repr (Lef u) u') -> repr d' (Rig u)) -> repr u u
   app :: repr u' (Lef u) -> repr u'' (Rig u) -> repr u u
+  defless :: repr d' u -> repr d u
 
 data V (repr :: Lo -> Lo -> *) where
   V :: (forall u' . repr (Lef u) u') -> V repr
@@ -27,12 +28,17 @@ data V (repr :: Lo -> Lo -> *) where
 data E (repr :: Lo -> Lo -> *) (use :: Lo) where
   E :: repr d u -> E repr u
 
+instance Show (E Easy u) where
+  show (E e) = show e
+
 toMy :: forall repr u . Lam repr => [(Name, V repr)] -> Exp -> E repr u
 toMy env (VarE (flip lookup env -> Just (V v))) = E v
 toMy env (AppE f a) = case (toMy env f, toMy env a) of (E f', E a') -> E (app f' a')
+toMy env (LamE [] e) = toMy env e
 toMy env (LamE [VarP nam] e) = E (lam f)
    where f :: (forall u' . repr (Lef u) u') -> repr d' (Rig u)
-         f v = case toMy ((nam, V v) : env) e of E e' -> e'
+         f v = case toMy ((nam, V v) : env) e of E e' -> defless e'
+toMy env (LamE (p:ps) e) = toMy env (LamE [p] $ LamE ps e)
 
 {-
 toMy :: Lam repr => [(Name, repr)] -> Exp -> repr
@@ -46,15 +52,17 @@ toMy env (LamE (p:ps) e) = toMy env (LamE [p] $ LamE ps e)
 instance Lam Easy where
   lam = Lam
   app = App
-
+  defless = Some
 
 data Easy :: Lo -> Lo -> * where
   It :: Easy (Lef u) u'
   App :: Easy u' (Lef u) -> Easy u'' (Rig u) -> Easy u u
   Lam :: ((forall u' . Easy (Lef u) u') -> Easy d' (Rig u)) -> Easy u u
+  Some :: Easy d' u -> Easy d u
 
 
 instance Show (Easy d u) where
   show It = "It"
   show (App f a) = "(" ++ show f ++ " " ++ show a ++ ")"
   show (Lam f) = "(\\" ++ show It ++ " -> " ++ show (f It) ++ ")"
+  show (Some e) = show e
