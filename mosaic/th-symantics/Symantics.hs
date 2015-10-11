55182fb7fcc0b24f8d7c795204ebb8db91c72a1d
Gabor Greif <ggreif@gmail.com>
2015-10-11 13:59:46 +0200

    WIP: start adding Known for locations

--- mosaic/th-symantics/Symantics.hs
+++ mosaic/th-symantics/Symantics.hs
@@ -19,20 +19,27 @@ data Lo = Root | Lef Lo | Rig Lo -- | Abs Loc
 
 class Lam (repr :: Lo -> Lo -> *) where
   --lam :: ((forall u' . repr (Lef u) u') -> repr d' (Rig u)) -> repr u u
-  lam :: ((forall u' . repr (Lef u) u') -> E repr (Rig u)) -> repr u u
+  lam :: ((forall u' . Known u' => repr (Lef u) u') -> E repr (Rig u)) -> repr u u
   app :: repr u' (Lef u) -> repr u'' (Rig u) -> repr u u
   --defless :: repr d' u -> repr d u
 
+
+class Known (loc :: Lo)
+
+instance Known Root
+instance Known loc => Known (Lef loc)
+instance Known loc => Known (Rig loc)
+
 data V (repr :: Lo -> Lo -> *) where
-  V :: (forall u' . repr (Lef u) u') -> V repr
+  V :: (forall u' . Known u' => repr (Lef u) u') -> V repr
 
 data E (repr :: Lo -> Lo -> *) (use :: Lo) where
-  E :: repr d u -> E repr u
+  E :: Known u => repr d u -> E repr u
 
 instance Show (E Easy u) where
   show (E e) = show e
 
-toMy :: forall repr u . Lam repr => [(Name, V repr)] -> Exp -> E repr u
+toMy :: forall repr u . (Known u, Lam repr) => [(Name, V repr)] -> Exp -> E repr u
 toMy env (VarE (flip lookup env -> Just (V v))) = E v
 toMy env (AppE f a) = case (toMy env f, toMy env a) of (E f', E a') -> E (app f' a')
 toMy env (LamE [] e) = toMy env e
