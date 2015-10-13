3af3785618d5c74a1c34275e8c42e45f4e68a101
Gabor Greif <ggreif@gmail.com>
2015-10-13 22:45:42 +0200

    fooling around

--- mosaic/th-symantics/Datatype.hs
+++ mosaic/th-symantics/Datatype.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE TemplateHaskell, ViewPatterns, KindSignatures, DataKinds, RankNTypes, GADTs, ScopedTypeVariables, FlexibleInstances, StandaloneDeriving #-}
+{-# LANGUAGE DatatypeContexts #-}
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
+import Control.Applicative -- for ghc 7.8
 
 -- GOAL: transform (simple) TH datatype splices to the format of level-aware inhabitants
 
 
-ex = [d| data Foo = A | B (Foo :: *) |]
+ex = [d| data Foo = A | B (Foo :: *) deriving Show |]
+ex0 = [d| data Foo a |]
+ex' = [d| data Foo (a :: Bool) = A | B a (Foo :: *) |]
+ex'' = [d| data (Show (a :: Bool), a ~ True) => Foo a = A | B a (Foo a) |] -- this needs DatatypeContexts
 
 pex = runQ ex >>= print
+pex0 = runQ ex0 >>= print
+pex' = runQ ex' >>= print
+pex'' = runQ ex'' >>= print
 
 
 mex = map (toMy []) <$> runQ ex >>= print
@@ -18,8 +26,10 @@ data Nat = Z | S Nat
 -- TODO: connect this to LC
 
 class Levelled repr where
-  star :: repr (S l)
-  inh :: repr (S l) -> (repr l -> repr l) -> repr l
+  star :: repr (S (S l))
+  --inh :: repr (S l) -> (repr l -> repr l) -> repr l
+  inh :: repr (S l) -> (repr l -> V repr) -> repr l
+  --inhs :: repr (S l) -> (Vec n (repr l) -> V repr) -> repr l -- should be
 
 instance Levelled repr -- FOR NOW!
 
@@ -30,4 +40,10 @@ data V :: (Nat -> *) -> * where
 instance Show (V repr)
 
 toMy :: Levelled repr => [(Name, V repr)] -> Dec -> V repr --- FOR NOW!
-toMy [] (DataD [] nam [] [] _) = V (star `inh` id)
+toMy [] (DataD [] nam [] [] _) = V (star `inh` V)
+toMy env (DataD [] nam tvs [] _) = V (star `inh` (\i -> inner env tvs))
+    where inner :: [(Name, V repr)] -> [TyVarBndr] -> V repr
+          inner env [] = undefined
+          inner env (PlainTV tvnam:tvs) = undefined
+
+-- TODO: tyvars should be "outer" and constructors should be "inner"
