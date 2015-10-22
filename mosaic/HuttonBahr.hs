f3042353924a7d618c20be227b71c98727b0c5a0
Gabor Greif <ggreif@gmail.com>
2015-10-22 10:26:26 +0200

    WIP: fused transform in leftist form
    does not compile!
    
    for now only one control stack constructor C0 is defined
    
    we need to flip stuff generally (this might resolve the scoping fixmes)

--- mosaic/HuttonBahr.hs
+++ mosaic/HuttonBahr.hs
@@ -79,17 +79,66 @@ eval' :: Exp -> (forall k . (Int -> k) -> k)
 eval' ((\case Lit n -> n; Add (eval -> a) (flip eval' (a+) -> b) -> b) -> res) c = c res
 
 -- maybe as two steps:
---   "  (eval -> b)   ... (<cont> b)"    ---->      "(<cont> . eval -> b)       ... (b)"
+--   "(eval -> b)   ... (<cont> b)"    ---->      "(<cont> . eval -> b)   ... (b)"
 --   "(<cont> . eval -> b)   ... (b)"    ---->      "(flip eval' <cont> -> b)   ... (b)"
 
 -- tackle side "a"...
--- step 0: isolate the continuation c'' of "a"
+-- step 1: isolate the continuation c'' of "a"
 eval' ((\case Lit n -> n; Add (eval -> a) ((\a' -> flip eval' (a'+)) a -> b) -> b) -> res) c = c res
--- step 1: chain it
+-- step 2: chain it
 eval' ((\case Lit n -> n; Add ((\a' -> flip eval' (a'+)) . eval -> a) (a -> b) -> b) -> res) c = c res
--- step 2: flip it in
+-- step 3: flip it in
 -}
 eval' ((\case Lit n -> n; Add (flip eval' (\a' -> flip eval' (a'+)) -> a) (a -> b) -> b) -> res) c = c res
 
 
 -- VICTORY!
+
+
+-- try the same thing for fused CPS-defunctionalisation
+
+-- characteristic equation
+
+eval'' x c = exec c (eval x)   -- better flipped: eval''_ c x = exec c (eval x)    ETA reduce:    eval''_ c = exec c . eval     ==>  flip eval'' c == exec c . eval
+-- leftise
+eval'' (eval -> res) c = exec c res
+-- leftise exec too
+eval'' (eval -> res) (exec -> work) = work res
+-- inline eval
+eval'' ((\case Lit n -> n; Add a b -> eval a + eval b) -> res) (exec -> work) = work res
+-- leftise twice
+eval'' ((\case Lit n -> n; Add (eval -> a) (eval -> b) -> a + b) -> res) (exec -> work) = work res
+-- find a c' for "  eval'' (eval -> b) c' = exec c' b   "
+-- cont for b is exec c'
+-- >> "  eval'' (eval -> b) c' = (exec c') b   "
+-- >> "  eval'' (exec c' . eval -> b') c' = b'   "   -- fixme: scoping issue for c'
+-- invent c' as exec (PLUS 
+
+--- RESTART
+-- what is the continuation for b? (exec c') == ?
+--  clearly (a+)
+eval'' ((\case Lit n -> n; Add (eval -> a) ((a+) . eval -> b) -> b) -> res) (exec -> work) = work res
+
+
+--- TRY harder
+-- observe in 
+eval'' ((\case Lit n -> n; Add (eval -> a) (eval -> b) -> a + b) -> res) (exec -> work) = work res
+-- res is used lineraly --> move it's continuation
+eval'' (work . (\case Lit n -> n; Add (eval -> a) (eval -> b) -> a + b) -> res') (exec -> work) = res'   -- fixme: scoping issue for "work"
+-- distribute in the arms:
+eval'' ((\case Lit n -> work n; Add (eval -> a) (eval -> b) -> work (a + b)) -> res') (exec -> work) = res'
+-- b is used linearly, tackle (eval -> b)
+-- find a c' for "  eval'' (eval -> b) c' = exec c' b  =  work (a + b) = exec c (a + b) "
+-- choose                                   exec c'@(C0 c a) b        := exec c (a + b)
+--        note "b" is used linearly here
+--        eval'' (eval -> b) c' = exec c'@(C0 c a) b
+--        eval'' (exec c' . eval -> b') c' = b -- fixme: scoping
+eval'' ((\case Lit n -> work n; Add (eval -> a) (eval -> b) -> exec (C0 c a) b) -> res') (exec -> work) = res'
+eval'' ((\case Lit n -> work n; Add (eval -> a) (exec (C0 c a) . eval -> b') -> b') -> res') (exec -> work) = res'
+-- flip equation
+eval'' ((\case Lit n -> work n; Add (eval -> a) (flip eval'' (C0 c a) -> b') -> b') -> res') (exec -> work) = res'
+
+
+
+
+exec = _
