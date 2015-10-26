a344be4b4b5d517e573aae8c94e1f2c072366a04
Gabor Greif <ggreif@gmail.com>
2015-10-26 18:36:32 +0100

    can we model runtime functions on the type level?
    first steps

---
+++ AddType.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ViewPatterns, KindSignatures, GADTs, PolyKinds #-}
+
+module AddType where
+
+data Nat = Z | S Nat deriving Show
+
+plus :: Nat -> Nat -> Nat
+plus Z = id
+plus (S (plus -> f)) = f . S
+
+--data Constr1 (arg :: Nat) (coarg :: Nat) where
+--  ConstrS :: Constr1 n (S n)
+data Constr1 (coarg :: Nat -> Nat) where
+  ConstrS :: Constr1 S
+
+data Plus (arg :: Nat) (result :: Nat -> Nat) where
+  PlusZ :: Id (f n) (f n) -> Plus n f
+  PlusS :: (Plus n `Compose` Constr1) f -> Plus (S n) f
+
+data Id (arg :: k) (coarg :: k) where
+  Id :: Id x x
+
+data Compose (a1 :: (b -> c) -> *) (a0 :: (a -> b) -> *) (coarg :: a -> c) where
+  --Compose
