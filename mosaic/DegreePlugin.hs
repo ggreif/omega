8bbc50a050e893c8bf3923c597cfe5a02ca60695
Gabor Greif <ggreif@gmail.com>
2016-02-17 15:14:38 +0100

    WIP: data families?

--- mosaic/DegreePlugin.hs
+++ mosaic/DegreePlugin.hs
@@ -1,20 +1,24 @@
 {-# LANGUAGE DataKinds, KindSignatures, PolyKinds, TypeFamilies, TypeOperators #-}
 
---type family (refin :: k) ° typ
---data (refin :: k) ° typ
-newtype (refin :: k) ° typ = Refined typ
+--type family (refin :: k)  typ
+--data (refin :: k)  typ
+--newtype (refin :: k)  typ = Refined typ
+data family (refin :: k)  typ --  = Refined typ
 
-test :: (Just a ° Maybe b) -> b
-test (Just a) = a
+newtype instance (Just a  Maybe b) = RefinedJust (Maybe b)
+newtype instance (Nothing  Maybe b) = RefinedNothing (Maybe b)
 
-test_aww :: (Just a ° Maybe b) -> (a ° b)
-test_aww (Just a) = a
+test :: (Just a  Maybe b) -> b
+test (RefinedJust (Just a)) = a
+
+test_aww :: (Just a  Maybe b) -> (a  b)
+test_aww (RefinedJust (Just a)) = undefined -- a
 
 
 -- PLAN: can we outsource the type checking of this to a plugin
 --       in the style of http://adam.gundry.co.uk/pub/typechecker-plugins/
 
--- (°) must be injective in the second arg
+-- () must be injective in the second arg
 -- implement the right refinement semantics
 
 -- Can a plugin see
