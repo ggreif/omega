9406598c39571b336f79893a0d3e869630aed361
Gabor Greif <ggreif@gmail.com>
2017-11-07 05:40:38 +0100

    sketch the idea

---
+++ mosaic/ForallConstructor.hs
@@ -0,0 +1,22 @@
+{-# language RankNTypes, TypeInType #-}
+
+
+-- * Idea
+-- Every rank-N forall gives rise to a type that only unifies with itself
+-- A value which has that type thus cannot be passed anywhere else.
+-- This pretty much parallels the definition of data/type constructors.
+-- So instead of having productions for defining constructors, why not
+-- only have those foralls and /consider/ them being constructors.
+
+-- ** Simplifying assumptions
+-- - Disregard levels for now
+-- - let's have one (open) kind and create type constructors
+-- - only have linear places (hrm, no places at all in the beginning)
+
+-- ** Task
+-- define in Univ something like
+--     data Foo
+--     data Bar
+
+class Univ u where
+  constr :: (forall t . u) -> u
